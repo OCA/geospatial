a4277fd38bea9391f9faa6cb5cc7faaf6bf63fe5
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2011-09-08 14:32:55 +0200

    [IMP] code cleanup before implementing view managment and better ir.model.fields creation

--- base_geoengine/geo_polygon.py
+++ base_geoengine/geo_polygon.py
@@ -14,5 +14,5 @@ class GeoPolygon(geo_field.Geom):
     def __init__(self, string, dim=2, srid=900913 , gist_index=True, **args):
         res = super(GeoPolygon, self).__init__(string, "POLYGON", **args)
         return res
-        
+
 fields.geo_polygon = GeoPolygon
