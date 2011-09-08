665b1341b349f963b60f8765650553d09fdb2d44
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2011-09-08 16:53:26 +0200

    [ADD] Layers definition structure

---
+++ base_geoengine/geo_view/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Nicolas Bessi. Copyright Camptocamp SA
+##############################################################################
+from . import geo_raster_layer
+from . import geo_vector_layer
+from . import ir_view
---
+++ base_geoengine/geo_view/ir_view.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Nicolas Bessi. Copyright Camptocamp SA
+##############################################################################
+from osv import fields, osv
+
+
+GEO_VIEW = ('geo_map_view', 'GeoEngine view definition')
+
+class IrUIView(osv.osv):
+    _inherit = 'ir.ui.view'
+
+    def __init__(self, pool, cursor):
+        """Hack due to the lack of selection fields inheritance mechanism."""
+        super(IrUIView, self).__init__(pool, cursor)
+        type_selection = self._columns['type'].selection
+        if GEO_VIEW not in type_selection:
+            tmp = list(type_selection)
+            tmp.append(GEO_VIEW)
+            tmp.sort()
+            self._columns['type'].selection = tuple(set(tmp))
+            
+    _columns = {'raster_layer_ids': fields.one2many('geoengine.raster.layer',
+                                                    'view_id',
+                                                    'Raster layers',
+                                                    required=False),
+
+                'vector_layer_ids': fields.one2many('geoengine.vector.layer',
+                                                    'view_id',
+                                                    'Vector layers',
+                                                    required=True)}
