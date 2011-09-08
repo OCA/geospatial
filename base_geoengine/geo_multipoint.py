144dc23cb22bc7f913f088cf7c7a7709ee6e6f7d
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2011-09-08 13:54:26 +0200

    [ADD] first commit

---
+++ base_geoengine/geo_multipoint.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Nicolas Bessi. Copyright Camptocamp SA
+##############################################################################
+
+from osv import fields, osv, orm
+from . import geo_field
+
+class GeoMultiPoint(geo_field.Geom):
+    """This class add a new type of columns to ORM it enable POSTGIS geometry type support"""
+    _type = 'geo_multi_point'
+    
+    def __init__(self, string, dim=2, srid=900913 , gist_index=True, **args):
+        res = super(GeoMultiPoint, self).__init__(string, "MULTIPOINT", **args)
+        return res
+        
+fields.geo_multi_point = GeoMultiPoint
