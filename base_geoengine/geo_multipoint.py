95082ecf063af2aac9224eed3568104dde72dbb4
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2011-09-08 14:32:55 +0200

    [IMP] code cleanup before implementing view managment and better ir.model.fields creation

--- base_geoengine/geo_multipoint.py
+++ base_geoengine/geo_multipoint.py
@@ -10,9 +10,9 @@ from . import geo_field
 class GeoMultiPoint(geo_field.Geom):
     """This class add a new type of columns to ORM it enable POSTGIS geometry type support"""
     _type = 'geo_multi_point'
-    
+
     def __init__(self, string, dim=2, srid=900913 , gist_index=True, **args):
         res = super(GeoMultiPoint, self).__init__(string, "MULTIPOINT", **args)
         return res
-        
+
 fields.geo_multi_point = GeoMultiPoint
