dfa24d39ac3dd82caa8bd0c1203c87c74fc56613
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2011-09-13 11:46:09 +0200

    [ADD] support of view definition without integritiy check

--- base_geoengine/geo_view/geo_raster_layer.py
+++ base_geoengine/geo_view/geo_raster_layer.py
@@ -17,17 +17,22 @@ class GeoRasterLayer(osv.osv):
                                                   ('openerp', 'OpenERP -- not implemented')],
                                                  string="Raster layer type",
                                                  required=True),
-                'name': fields.char('Layer Name', size=256, required=True),
+                'name': fields.char('Layer Name', size=256, translate=True, required=True),
                 'url': fields.text('Service URL'),
                 'google_type':  fields.selection([('G_NORMAL_MAP', 'Google normal map'),
                                                   ('G_SATELLITE_MAP', 'Google staellite map'),
                                                   ('G_HYBRID_MAP', 'Google Hybrid map'),
                                                   ('G_PHYSICAL_MAP', 'Google Physical map')],
-                                                 string="Google raster layer type",
-                                                 required=True),
+                                                 string="Google raster layer type"),
                 'sequence': fields.integer('layer priority lower on top'),
                 'overlay' : fields.boolean('Is overlay layer?'),
                 'field_id': fields.many2one('ir.model.fields', 'OpenERP layer field to use',
-                                            domain=[('ttype', 'ilike', 'geo_')]),
-                'view_id' : fields.many2one('ir.ui.view', 'Related View', required=True)}
+                                            domain=[('ttype', 'ilike', 'geo_'),
+                                                    ('model', '=', 'view_id.model')]),
+                'view_id' : fields.many2one('ir.ui.view', 'Related View',
+                                             domain=[('type', '=', 'geo_map_view')],
+                                             required=True)}
+# TODO Write data check consraints
 GeoRasterLayer()
+
+_defaults = {'sequence': lambda *a: 6}
