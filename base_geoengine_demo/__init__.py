89b77a569589e0fb413edd8505cfc535eb4c5b14
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2012-02-02 00:21:41 +0100

    [ADD] geoname bulk wizzard + small fixes

--- base_geoengine_demo/__init__.py
+++ base_geoengine_demo/__init__.py
@@ -1,2 +1,10 @@
+#-*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2010 Camptocamp SA (http://www.camptocamp.com) 
+# @autor Nicolas Bessi (nbessi)
+# All Right Reserved
+#
+##############################################################################
 from . import geo_npa
 from . import retail_machine
