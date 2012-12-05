ca64d0eb8eaa4b8378b5d29a52fba6acf5ef884a
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2012-12-05 11:29:37 +0100

    [FIX] call to super in geo fields

--- base_geoengine/geo_point.py
+++ base_geoengine/geo_point.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from __future__ import absolute_import 
+from __future__ import absolute_import
 from osv import fields
 from . import geo_field
 
@@ -26,7 +26,8 @@ class GeoPoint(geo_field.Geom):
     """New type of column in the  ORM for POSTGIS geometry Point type"""
     _type = 'geo_point'
     def __init__(self, string, dim=2, srid=900913 , gist_index=True, **args):
-        res = super(GeoPoint, self).__init__(string, "POINT", **args)
+        res = super(GeoPoint, self).__init__(string, "POINT", dim=dim,
+                                             srid=srid, gist_index=gist_index, **args)
         return res
 
 fields.geo_point = GeoPoint
--- base_geoengine/geo_polygon.py
+++ base_geoengine/geo_polygon.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from __future__ import absolute_import 
+from __future__ import absolute_import
 from osv import fields
 from . import geo_field
 
@@ -27,7 +27,8 @@ class GeoPolygon(geo_field.Geom):
     _type = 'geo_polygon'
 
     def __init__(self, string, dim=2, srid=900913 , gist_index=True, **args):
-        res = super(GeoPolygon, self).__init__(string, "POLYGON", **args)
+        res = super(GeoPolygon, self).__init__(string, "POLYGON",
+                                               dim=dim, srid=srid, gist_index=gist_index, **args)
         return res
 
 fields.geo_polygon = GeoPolygon
