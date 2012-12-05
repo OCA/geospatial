d31dcb8910d4d9177a833e30964156a08b57597d
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2012-12-05 11:29:37 +0100

    [FIX] call to super in geo fields

--- base_geoengine/geo_multipoint.py
+++ base_geoengine/geo_multipoint.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from __future__ import absolute_import 
+from __future__ import absolute_import
 from osv import fields
 from . import geo_field
 
@@ -27,7 +27,8 @@ class GeoMultiPoint(geo_field.Geom):
     _type = 'geo_multi_point'
 
     def __init__(self, string, dim=2, srid=900913 , gist_index=True, **args):
-        res = super(GeoMultiPoint, self).__init__(string, "MULTIPOINT", **args)
+        res = super(GeoMultiPoint, self).__init__(string, "MULTIPOINT",
+                                                  dim=dim, srid=srid, gist_index=gist_index, **args)
         return res
 
 fields.geo_multi_point = GeoMultiPoint
