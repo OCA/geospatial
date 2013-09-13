0fd7fc3f897117b9a3fd527ac53c17045b0838bc
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-09-13 16:48:29 -0400

    Fixed warnings and view. Still has js errors.

--- base_geoengine/__openerp__.py
+++ base_geoengine/__openerp__.py
@@ -52,7 +52,7 @@
         "static/src/js/geoengine_edit_widget.js"],
  'css': ["static/src/css/style.css",],
  'qweb' : ["static/src/xml/*.xml"],
- 'installable': False,
+ 'installable': True,
  'active': False,
  'icon': '/base_geoengine/static/src/images/map_icon.png'}
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
