432cab5740da8170c1fe13d5b69d570d9c1a6d35
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-09-25 17:50:12 -0400

    [FIX] Made geo_partner installable

--- geoengine_partner/geo_partner.py
+++ geoengine_partner/geo_partner.py
@@ -19,41 +19,11 @@
 #
 ##############################################################################
 
-from osv import fields, osv
-
+from osv import fields
 from base_geoengine import geo_model
-
-class ResPartnerAddress(geo_model.GeoModel):
-    """Add geo_point to addresses"""
-    _name = "res.partner.address"
-    _inherit = "res.partner.address"
-    _columns = {'geo_point' : fields.geo_point('Addresses coordinate')}
-ResPartnerAddress()
 
 class ResPartner(geo_model.GeoModel):
     """Add geo_point to partner using a function filed"""
     _name = "res.partner"
     _inherit = "res.partner"
-    
-    def _get_point(self, cursor, uid, partner_ids, name, args, context=None):
-        """ Return Json repr of field"""
-        res = {}
-        add_obj = self.pool.get('res.partner.address')
-        context = context or {}
-        if not isinstance(partner_ids, list):
-            partner_ids = [partner_ids]
-        for partner_id in partner_ids:
-            add = self.address_get(cursor, uid, [partner_id], adr_pref=['invoice'])
-            if add.get('invoice'):
-                res[partner_id] = add_obj.browse(cursor, uid, add.get('invoice'), context).geo_point
-            else:
-               res[partner_id] = False 
-        return res
-            
-    _columns = {'geo_point' : fields.geo_function(_get_point, 
-                                                  string='Partner coordinates',
-                                                  type='geo_point',
-                                                  srid=900913, # mandatory arg
-                                                  dim=2)} # mandatory arg
-ResPartner()
-
+    _columns = {'geo_point' : fields.geo_point('Addresses coordinate')}
