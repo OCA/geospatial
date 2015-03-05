625dc7e467b7785a6d1dabd99cadf8a64b74cc5d
Laurent Mignon (aka lmi) <laurent.mignon@acsone.eu>
2015-03-05 18:58:57 +0100

    [FIX] Uses the new API

--- base_geoengine/geo_multipoint.py
+++ base_geoengine/geo_multipoint.py
@@ -25,10 +25,11 @@ from . import geo_field
 class GeoMultiPoint(geo_field.Geom):
     """New type of column in the  ORM for POSTGIS geometry MultiPoint type"""
     _type = 'geo_multi_point'
+    _geo_type = 'MULTIPOINT'
 
     def __init__(self, string, dim=2, srid=900913, gist_index=True, **args):
         super(GeoMultiPoint, self).__init__(
-            string, "MULTIPOINT", dim=dim, srid=srid, gist_index=gist_index,
+            string, dim=dim, srid=srid, gist_index=gist_index,
             **args)
 
 fields.geo_multi_point = GeoMultiPoint
