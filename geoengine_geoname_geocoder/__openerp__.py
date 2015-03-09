62c316dc0fbdfa575f2554222057598923e1fec3
Jordi Riera <jordi.riera@savoirfairelinux.com>
2015-03-09 14:28:14 -0400

    [IMP] Move icons to static/description.

--- geoengine_geoname_geocoder/__openerp__.py
+++ geoengine_geoname_geocoder/__openerp__.py
@@ -43,5 +43,5 @@
  'depends': ['base', 'sale', 'geoengine_partner'],
  'installable': True,
  'active': False,
- 'icon': '/base_geoengine/static/src/images/map_icon.png'}
+ }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
