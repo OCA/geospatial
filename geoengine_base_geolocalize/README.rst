16b31f45b33329df39470af2a3acf086d21278f6
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2015-03-10 20:14:00 +0100

    [ADD] New module extending base_geolocalize The modul add a new field 'geo_point' computed from partner_latitue and partner_longitude added by base_geolocalize. The geo_point is rendered in a map on the partner form

---
+++ geoengine_base_geolocalize/README.rst
@@ -0,0 +1,35 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+
+Geospatial support for base_geolocalize
+=======================================
+
+Geolocalise your partner based on longitude and latitude provided by base_geolocalize.
+
+
+Installation
+============
+
+Take a look at the installation section in the description of the module 
+'base_geoengine'.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Laurent Mignon <laurent.mignon@acsone.eu>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
---
+++ geoengine_base_geolocalize/models/res_partner.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Author: Laurent Mignon
+#   Copyright (c) 2015 Acsone SA/NV (http://www.acsone.eu)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api
+
+from openerp.addons.base_geoengine import geo_model
+from openerp.addons.base_geoengine import fields
+
+
+class ResPartner(geo_model.GeoModel):
+    """Add geo_point to partner using a function field"""
+    _inherit = "res.partner"
+
+    @api.depends('partner_latitude', 'partner_longitude')
+    def _get_geo_point(self):
+        if not self.partner_latitude or not self.partner_longitude:
+            self.geo_point = False
+        self.geo_point = fields.GeoPoint.from_latlon(
+            self.env.cr, self.partner_latitude, self.partner_longitude)
+
+    geo_point = fields.GeoPoint(
+        readonly=True, store=True, compute='_get_geo_point')
