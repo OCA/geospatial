b98d9c3192808c856f059946d89c163613410e82
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2015-05-26 10:08:23 +0200

    Style

--- base_geoengine/fields.py
+++ base_geoengine/fields.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 import logging
 try:
     from shapely.geometry import Point
@@ -39,7 +57,7 @@ class GeoField(Field):
     @classmethod
     def load_geo(cls, wkb):
         """Load geometry into browse record after read was done"""
-        return wkb and wkbloads(wkb.decode('hex')) or False
+        return wkbloads(wkb.decode('hex')) if wkb else False
 
 
 class GeoLine(GeoField):
