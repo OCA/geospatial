fed3860fd7078e3e5ecfbfe25eb79ed1a9198592
Laurent Mignon <laurent.mignon@acsone.eu>
2015-06-29 00:05:26 +0200

    chromojs

--- base_geoengine/static/src/js/views/geoengine_view.js
+++ base_geoengine/static/src/js/views/geoengine_view.js
@@ -202,10 +202,55 @@ var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
                 );
             }
         });
-        /*var indicator = cfg.attribute_field_id[1];
+        styleInfo = self.styleVectorLayer(cfg, data);
+        var lv = new ol.layer.Vector({
+                    source: vectorSource,
+                    title: cfg.name,
+                    opacity: 0.8,
+                    style: styleInfo.style,
+                    legend: styleInfo.legend
+        });
+        lv.on('change:visible', function(e){
+            el = self.$('#map_legend');
+            el.hide();
+            if(lv.getVisible()){
+                el.html(lv.get('legend'))
+                el.show();
+            }
+        });
+        return lv;
+    },
+
+    styleVectorLayer: function(cfg, data) {
+        var self = this;
+        var indicator = cfg.attribute_field_id[1];
         switch (cfg.geo_repr) {
             case "basic":
-                if (!cfg.symbols) {
+                var fill = new ol.style.Fill({
+                    color: cfg.begin_color
+                });
+                var stroke = new ol.style.Stroke({
+                    color: '#333333',
+                    width: 1
+                });
+                var styles = [
+                    new ol.style.Style({
+                        image: new ol.style.Circle({
+                            fill: fill,
+                            stroke: stroke,
+                            radius: 5,
+                        }),
+                        fill: fill,
+                        stroke: stroke
+                    })
+                ];
+                return {
+                    style: function(feature, resolution) {
+                        return styles;
+                    },
+                    legend: ''
+                };
+                /*if (!cfg.symbols) {
                     break;
                 }
                 if (cfg.symbols.length > 0) {
@@ -213,135 +258,103 @@ var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
                     vl.styleMap = new OpenLayers.StyleMap({
                         'default': style,
                     });
-                }
-                break;
+                } */
             case "colored":
-                var begin_color = new mapfish.ColorRgb();
-                begin_color.setFromHex(cfg.begin_color || DEFAULT_BEGIN_COLOR);
-                var end_color = new mapfish.ColorRgb();
-                end_color.setFromHex(cfg.end_color || DEFAULT_END_COLOR);
+                var begin_color = cfg.begin_color || DEFAULT_BEGIN_COLOR;
+                var end_color = cfg.end_color || DEFAULT_END_COLOR;
+                var values = [];
+                _.each(data, function(item) {
+                    values.push(item[indicator]);
+                });
+                var nb_class = cfg.nb_class || DEFAULT_NUM_CLASSES
+                var scale = chroma.scale([begin_color, end_color]);
+                var serie = new geostats(values);
                 switch (cfg.classification) {
                     case "unique":
-                        vl.styleMap = this.getUniqueValuesStyleMap(cfg, features);
+                        var vals = serie.getClassUniqueValues();
+                        scale = chroma.scale('RdYlBu').domain([0, vals.length], vals.length);
                         break;
                     case "quantile":
-                        geostat = new mapfish.GeoStat.Choropleth(map, {
-                            method: mapfish.GeoStat.Distribution.CLASSIFY_BY_QUANTILS,
-                            numClasses: cfg.nb_class || DEFAULT_NUM_CLASSES,
-                            colors: [ begin_color, end_color ],
-                            layer: vl,
-                            indicator: indicator,
-                            featureSelection: false
-                        });
+                        serie.getClassQuantile(nb_class);
+                        var vals = serie.getRanges();
+                        scale = scale.domain([0, vals.length], vals.length);
                         break;
                     case "interval":
-                        geostat = new mapfish.GeoStat.Choropleth(map, {
-                            method: mapfish.GeoStat.Distribution.CLASSIFY_BY_EQUAL_INTERVALS,
-                            numClasses: cfg.nb_class || DEFAULT_NUM_CLASSES,
-                            colors: [ begin_color, end_color ],
-                            layer: vl,
-                            indicator: indicator,
-                            featureSelection: false
-                        });
+                        serie.getClassEqInterval(nb_class);
+                        var vals = serie.getRanges();
+                        scale = scale.domain([0, vals.length], vals.length);
                         break;
                 }
+                var colors = scale.colors(mode='hex');
+                serie.setColors(colors);
+                var styles_map = {};
+                _.each(colors, function(color) {
+                    if (color in styles_map) {
+                        return;
+                    }
+                    var fill = new ol.style.Fill({
+                        color: color
+                    });
+                    var stroke = new ol.style.Stroke({
+                        //color: color,
+                        color: '#333333',
+                        width: 1
+                    });
+                    var styles = [
+                        new ol.style.Style({
+                          image: new ol.style.Circle({
+                            fill: fill,
+                            stroke: stroke,
+                            radius: 5
+                          }),
+                          fill: fill,
+                          stroke: stroke
+                        })
+                    ];
+                    styles_map[color] = styles;
+                });
+                return {
+                    style : function(feature, resolution) {
+                        var value = feature.get('attributes')[indicator];
+                        var color_idx = self.getClass(value, vals);
+                        return styles_map[colors[color_idx]];
+                     },
+                    legend: serie.getHtmlLegend(null, cfg.name, 1)
+                };
                 break;
             case "proportion":
-                geostat = new mapfish.GeoStat.ProportionalSymbol(map, {
+                /*geostat = new mapfish.GeoStat.ProportionalSymbol(map, {
                     layer: vl,
                     indicator: indicator,
                     //TODO: this should not be hardcoded!
                     minSize: cfg.min_size || DEFAULT_MIN_SIZE,
                     maxSize: cfg.max_size || DEFAULT_MAX_SIZE,
                     featureSelection: false
-                });
+                });*/
                 break;
         }
-        vl.addFeatures(features);
-        if (geostat instanceof mapfish.GeoStat) {
-            geostat.setClassification();
-            geostat.applyClassification();
-        }*/
-
-        var fill = new ol.style.Fill({
-            color: cfg.begin_color
-        });
-        var stroke = new ol.style.Stroke({
-            color: '#3399CC',
-            width: 1.25
-        });
-        var styles = [
-            new ol.style.Style({
-              image: new ol.style.Circle({
-                fill: fill,
-                stroke: stroke,
-                radius: 5
-              }),
-              fill: fill,
-              stroke: stroke
-            })
-        ];
-        var lv = new ol.layer.Vector({
-            source: vectorSource,
-            title: cfg.name,
-            style: function(feature, resolution) {
-               return styles;
-            },
-            opacity: 0.5,
-         });
-         return lv;
     },
 
-    getUniqueValuesStyleMap: function(cfg, features) {
-        if (jQuery.isEmptyObject(features)) {
-            return;
+    getClass :function(val, a) {
+        // uniqueValues classification
+        idx = a.indexOf(val);
+        if (idx > -1){
+            return idx;
         }
-        var l = features.length, f = features[0];
-        var v = cfg.attribute_field_id[1];
-
-        // get the number of unique values
-        var distinct = {};
-        var i = 0;
-        do {
-            f = features[i];
-            i += 1;
-        } while (!f.attributes.hasOwnProperty(v));
-        // ok, we've found a feature whose attribute is present
-        distinct[f.attributes[v].toString()] = null;
-        var nb = 1;
-        while(l-- && l>=i) {
-            f = features[l];
-            if (!distinct.hasOwnProperty(f.attributes[v])) {
-                distinct[f.attributes[v]] = null;
-                nb += 1;
+        // range classification
+        var separator = ' - '
+        for(var i= 0; i < a.length; i++) {
+            // all classification except uniqueValues
+            if(a[i].indexOf(separator) != -1) {
+                var item = a[i].split(separator);
+                if(val <= parseFloat(item[1])) {return i;}
+            } else {
+                // uniqueValues classification
+                if(val == a[i]) {
+                    return i;
+                }
             }
         }
-        // define the nb different colors
-        var colors = utils.color.generateDistinctColors(nb);
-        // associate colors with values
-        l = 0;
-        for (var key in distinct) {
-            if (distinct.hasOwnProperty(key)) {
-                distinct[key] = colors[l];
-                l += 1;
-            }
-        }
-        // display them
-        return new OpenLayers.StyleMap({
-            'default': new OpenLayers.Style(
-                OpenLayers.Util.applyDefaults({
-                    fillColor: "${color}"
-                }, STYLE_DEFAULT),
-                {
-                    context: {
-                        color: function(f) {
-                            return (f.attributes.hasOwnProperty(v) && distinct.hasOwnProperty(f.attributes[v])) ?
-                                '#'+distinct[f.attributes[v]] : "#ffffff";
-                        }
-                    }
-                }),
-            'select': STYLE_SELECT
-        });
     },
 
     /**
@@ -446,7 +459,7 @@ var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
                 target: 'olmap',
                 view: new ol.View({
                     center: [0, 0],
-                    zoom: 5
+                    zoom: 2
                 }),
                 controls: ol.control.defaults().extend([
                     new ol.control.FullScreen(),
@@ -458,28 +471,6 @@ var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
             map.addControl(layerSwitcher);
             this.map = map;
             this.register_interaction();
-            /*OpenLayers.ImgPath = "//dr0duaxde13i9.cloudfront.net/theme/dark/";
-            this.map = new OpenLayers.Map("the_map", {
-                layers: this.createBackgroundLayers(this.fields_view.geoengine_layers.backgrounds),
-                displayProjection: new OpenLayers.Projection("EPSG:4326"), // Fred should manage projection here
-                theme: null,
-                controls: [
-                    new OpenLayers.Control.KeyboardDefaults(),
-                    new OpenLayers.Control.ZoomBox(),
-                    new OpenLayers.Control.Attribution(),
-                    new OpenLayers.Control.LayerSwitcher({roundedCornerColor: 'black'}),
-                    new OpenLayers.Control.PanZoomBar(),
-                    new OpenLayers.Control.ToolPanel()
-                ]
-            });
-            // set z-index of container to be under search view z-index of 666
-            this.map.layerContainerDiv.style.zIndex = 600;
-            if (this.restricted_extent) {
-                this.map.restrictedExtent = OpenLayers.Bounds.fromString(this.restricted_extent);
-                if (this.projection != this.map.getProjection()) {
-                    this.map.restrictedExtent = this.map.restrictedExtent.transform(this.projection, this.map.getProjection());
-                }
-            }*/
         }
     },
 
@@ -504,105 +495,6 @@ var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
     },
 
 });
-// here you may tweak globals object, if any, and play with on_* or do_* callbacks on them
-
-var utils = {
-    color: {
-
-        colorPalette: [
-            "00FFFF", "FF00FF", "000080", "00FF00", "FFFF00", "FF0000",
-            "0000FF", "008080", "FFB300", "803E75", "FF6800", "A6BDD7",
-            "C10020", "CEA262", "817066", "007D34", "F6768E", "FF7A5C",
-            "53377A", "B32851", "93AA00", "593315", "F13A13", "232C16"
-        ],
-
-        /**
-        * Converts an HSV color value to RGB. Conversion formula
-        * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
-        * Assumes h, s, and v are contained in the set [0, 1] and
-        * returns r, g, and b in the set [0, 255].
-        *
-        * @param   Number  h       The hue
-        * @param   Number  s       The saturation
-        * @param   Number  v       The value
-        * @return  Array           The RGB representation
-        */
-        hsvToRgb: function(h, s, v) {
-            var r, g, b;
-
-            var i = Math.floor(h * 6);
-            var f = h * 6 - i;
-            var p = v * (1 - s);
-            var q = v * (1 - f * s);
-            var t = v * (1 - (1 - f) * s);
-
-            switch(i % 6) {
-                case 0: r = v, g = t, b = p; break;
-                case 1: r = q, g = v, b = p; break;
-                case 2: r = p, g = v, b = t; break;
-                case 3: r = p, g = q, b = v; break;
-                case 4: r = t, g = p, b = v; break;
-                case 5: r = v, g = p, b = q; break;
-            }
-
-            return [r * 255, g * 255, b * 255];
-        },
-
-        /**
-        * Converts an RGB color value to Hex.
-        * Assumes r, g, and b are contained in the set [0, 255]
-        *
-        * @param   Number  r       The red color value
-        * @param   Number  g       The green color value
-        * @param   Number  b       The blue color value
-        * @return  Array           The HSV representation
-        */
-        rgbToHex: function(r, g, b) {
-            var colorBase10 = (r + 256 * g + 256 * 256 * b).toString(16);
-            while (colorBase10.length < 6) {
-                colorBase10 = '0' + colorBase10;
-            }
-            return colorBase10;
-        },
-
-        /**
-        * Method: generateDistinctColors
-        *
-        * Parameters:
-        * nb - {integer} number of distinct colors to generate (max 768)
-        */
-        generateDistinctColors: function(nb) {
-            var palette = utils.color.colorPalette;
-            // use the static palette is few colors else dynamically generate a new
-            // palette
-            if (nb <= palette.length) {
-                return palette.slice(0, nb);
-            } else {
-                var h, s, v; // hue, value, saturation of the HSV color space
-                var SV_RANGE = 0.6; // empiric value
-                var SV_MAXVALUE = 0.9; // empiric value
-                var HUE_NB_VARIATIONS_PER_SV = 20; // empiric value
-                var sv_variation = Math.ceil(nb/HUE_NB_VARIATIONS_PER_SV);
-                var it; // iterator which value is: 0 <= it <= sv_variation
-                var out = [];
-                for (var i=0; i<nb; i+=1) {
-                    h = i/nb;
-                    it = i % sv_variation;
-                    s = v = (it == sv_variation-1) ?
-                        SV_MAXVALUE : SV_MAXVALUE - it*(SV_RANGE/(sv_variation-1));
-                    out.push(this.rgbToHex.apply(this, _.map(
-                        this.hsvToRgb(i/nb, s, v),
-                        function(num) {
-                            return Math.round(num);
-                        })
-                    ));
-                }
-                return out;
-
-            }
-        }
-    }
-};
 
 core.view_registry.add('geoengine', GeoengineView);
 
