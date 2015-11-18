9e473f939a9d028d564858fc92e17140a766af4b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-11-18 10:41:41 +0100

    [MIGR] Separate common function for widget and view to create layers in a mixin abstract model

--- base_geoengine/static/src/js/views/geoengine_view.js
+++ base_geoengine/static/src/js/views/geoengine_view.js
@@ -17,6 +17,8 @@ var core = require('web.core');
 var time = require('web.time');
 var View = require('web.View');
 
+var geoengine_common = require('base_geoengine.geoengine_common');
+
 var _lt = core._lt;
 var QWeb = core.qweb;
 
@@ -76,88 +78,7 @@ var formatHTML = function(a) {
     return str.join('<br />');
 };
 
-/**
- * Method: createBackgroundLayers
- * creates background layers from config
- *
- * Parameters:
- * bg_layers - {Array} the background layers array of config objects
- */
-var createBackgroundLayers = function(bg_layers) {
-    var out = [];
-    _.each(bg_layers, function(l) {
-        switch (l.raster_type) {
-            case "osm":
-                out.push(
-                    new OpenLayers.Layer.OSM(
-                        l.name,
-                        'http://tile.openstreetmap.org/${z}/${x}/${y}.png', {
-                            attribution: "<a href='http://www.camptocamp.com' style='color:orange;font-weight:bold;background-color:#FFFFFF' target='_blank'>Powered by Camptocamp</a>\
-                                          using <a href='http://www.openstreetmap.org/' target='_blank'>OpenStreetMap</a> raster",
-                            buffer: 1,
-                            transitionEffect: 'resize'
-                        }));
-                break;
-            case "mapbox":
-                out.push(
-                    new OpenLayers.Layer.XYZ(l.name, [
-                        "http://a.tiles.mapbox.com/v3/" + l.mapbox_id + "/${z}/${x}/${y}.png",
-                        "http://b.tiles.mapbox.com/v3/" + l.mapbox_id + "/${z}/${x}/${y}.png",
-                        "http://c.tiles.mapbox.com/v3/" + l.mapbox_id + "/${z}/${x}/${y}.png",
-                        "http://d.tiles.mapbox.com/v3/" + l.mapbox_id + "/${z}/${x}/${y}.png"
-                    ], {
-                        sphericalMercator: true,
-                        wrapDateLine: true,
-                        numZoomLevels: 17,
-                        attribution: "<a href='http://www.camptocamp.com' style='color:orange;font-weight:bold;background-color:#FFFFFF' target='_blank'>Powered by Camptocamp</a>\
-                                  using <a href='http://www.openstreetmap.org/' target='_blank'>OpenStreetMap</a> raster"
-                    })
-                );
-                break;
-            case "swisstopo":
-                var resolutions = [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5];
-                if (l.swisstopo_type == 'ch.swisstopo.swissimage') {
-                    resolutions.push(2, 1.5, 1, 0.5);
-                }
-                out.push(
-                    new OpenLayers.Layer.WMTS({
-                        name: l.name,
-                        layer: l.swisstopo_type,
-                        formatSuffix: 'jpeg',
-                        url: ['https://wmts0.geo.admin.ch/', 'https://wmts1.geo.admin.ch/', 'https://wmts2.geo.admin.ch/'],
-                        projection: 'EPSG:21781',
-                        units: 'm',
-                        resolutions: resolutions,
-                        maxExtent: [420000, 30000, 900000, 350000],
-                        requestEncoding: 'REST',
-                        matrixSet: '21781',
-                        style: 'default',
-                        dimensions: ['TIME'],
-                        params: {time: l.swisstopo_time},
-                        attribution: "<a href='http://www.camptocamp.com' style='color:orange;font-weight:bold;background-color:#FFFFFF' target='_blank'>Powered by Camptocamp</a>\
-                                  data <a href='http://www.swisstopo.admin.ch/' target='_blank'>&copy; swisstopo</a>"
-                    })
-                );
-                break;
-            case "google":
-                var glayers = {
-                    "G_PHYSICAL_MAP": google.maps.MapTypeId.TERRAIN,
-                    "G_HYBRID_MAP": google.maps.MapTypeId.HYBRID,
-                    "G_SATELLITE_MAP": google.maps.MapTypeId.SATELLITE
-                };
-                out.push(
-                    new OpenLayers.Layer.Google(
-                        l.name,
-                        {type: glayers[l.google_type],
-                        attribution: "<a href='http://www.camptocamp.com' style='position:relative;left:-470px;color:orange;font-weight:bold;background-color:#FFFFFF' target='_blank'>Powered by Camptocamp</a>"}
-                    ));
-                break;
-        }
-    });
-    return out;
-};
-
-var GeoengineView = View.extend({
+var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
     template: "GeoengineView",
     display_name: _lt('Geoengine'),
     icon: 'fa-map-o',
@@ -444,7 +365,7 @@ var GeoengineView = View.extend({
         if (_.isUndefined(this.map)){
             OpenLayers.ImgPath = "//dr0duaxde13i9.cloudfront.net/theme/dark/";
             map = new OpenLayers.Map('the_map', {
-                layers: createBackgroundLayers(this.fields_view.geoengine_layers.backgrounds),
+                layers: this.createBackgroundLayers(this.fields_view.geoengine_layers.backgrounds),
                 displayProjection: new OpenLayers.Projection("EPSG:4326"), // Fred should manage projection here
                 theme: null,
                 controls: [
@@ -572,103 +493,4 @@ var utils = {
 core.view_registry.add('geoengine', GeoengineView);
 
 return GeoengineView;
-});
-
-OpenLayers.Control.ToolPanel = OpenLayers.Class(OpenLayers.Control.Panel, {
-    initialize: function(options) {
-        OpenLayers.Control.Panel.prototype.initialize.apply(this, [options]);
-
-        var style = new OpenLayers.Style();
-        style.addRules([
-            new OpenLayers.Rule({symbolizer: {
-                "Point": {
-                    pointRadius: 4,
-                    graphicName: "square",
-                    fillColor: "white",
-                    fillOpacity: 1,
-                    strokeWidth: 1,
-                    strokeOpacity: 1,
-                    strokeColor: "#000000"
-                },
-                "Line": {
-                    strokeWidth: 3,
-                    strokeOpacity: 1,
-                    strokeColor: "#000000",
-                    strokeDashstyle: "dash"
-                },
-                "Polygon": {
-                    strokeWidth: 2,
-                    strokeOpacity: 1,
-                    strokeColor: "#000000",
-                    fillColor: "white",
-                    fillOpacity: 0.3
-                }
-            }})]);
-
-        var styleMap = new OpenLayers.StyleMap({"default": style});
-
-        this.displayClass = 'olControlEditingToolbar';
-        var navCtrl = new OpenLayers.Control.Navigation();
-        this.defaultControl = navCtrl;
-        this.addControls([
-            new OpenLayers.Control.Measure(
-                OpenLayers.Handler.Polygon, {
-                    persist: true,
-                    immediate: true,
-                    displayClass: "olControlDrawFeaturePolygon",
-                    title: "Measure an area",
-                    handlerOptions: {
-                        layerOptions: {styleMap: styleMap}
-                    },
-                    eventListeners: {
-                        "measure": OpenLayers.Function.bind(this.handleFullMeasurements, this),
-                        "measurepartial": OpenLayers.Function.bind(this.handlePartialMeasurements, this),
-                        "deactivate": function() {
-                            document.getElementById('map_measurementbox').style.display = 'none';
-                        }
-                    }
-                }),
-            new OpenLayers.Control.Measure(
-                OpenLayers.Handler.Path, {
-                    persist: true,
-                    immediate: true,
-                    displayClass: "olControlDrawFeaturePath",
-                    title: "Measure a distance",
-                    handlerOptions: {
-                        layerOptions: {styleMap: styleMap}
-                    },
-                    eventListeners: {
-                        "measure": OpenLayers.Function.bind(this.handleFullMeasurements, this),
-                        "measurepartial": OpenLayers.Function.bind(this.handlePartialMeasurements, this),
-                        "deactivate": function() {
-                            document.getElementById('map_measurementbox').style.display = 'none';
-                        }
-                    }
-                }),
-            navCtrl
-        ]);
-    },
-    handlePartialMeasurements: function(event) {
-        this.handleMeasurements(event);
-    },
-    handleFullMeasurements: function(event) {
-        this.handleMeasurements(event);
-    },
-    handleMeasurements: function(event) {
-        var geometry = event.geometry;
-        var units = event.units;
-        var order = event.order;
-        var measure = event.measure;
-        var element = document.getElementById('map_measurementbox');
-        var out = "";
-        if (order == 1) {
-            out += '<span style="font-weight: bold">measure</span>: ' + measure.toFixed(1) + " " + units;
-        } else {
-            out += '<span style="font-weight: bold">measure</span>: ' + measure.toFixed(1) + " square " + units;
-        }
-        element.innerHTML = out;
-        element.style.display = 'block';
-    },
-    CLASS_NAME: "OpenLayers.Control.ToolPanel"
-
 });
