67050b32f0b186cd890395f562f296bd7e39edba
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-22 16:51:33 +0100

    [FIX] display translation of field string instead of field name and display values of selection field instead of technical value in info map box in geoengine view

--- base_geoengine/static/src/js/geoengine_view.js
+++ base_geoengine/static/src/js/geoengine_view.js
@@ -38,7 +38,7 @@ openerp.base_geoengine = function(openerp) {
      * Parameters:
      * a - {Object}
      */
-    var formatHTML = function(a) {
+    var formatHTML = function(a, fields) {
         var str = [];
         var oid = '';
         for (var key in a) {
@@ -47,15 +47,28 @@ openerp.base_geoengine = function(openerp) {
                 if (val == false) {
                     continue;
                 }
-                var label = '';
-                if (val instanceof Array) {
-                    str.push('<span style="font-weight: bold">' + key.charAt(0).toUpperCase() + key.slice(1) + '</span>: ' +val[1]);
-                } else {
-                    label = '<span style="font-weight: bold">' + key.charAt(0).toUpperCase() + key.slice(1) + '</span>: ' +val;
-                     if (key == 'id') {
-                        oid = label;
+                var span = '';
+                if (fields.hasOwnProperty(key)) {
+                    var field = fields[key];
+                    var label = field.string;
+                    if (field.type == 'selection') {
+                        // get display value of selection option
+                        for (var option in field.selection) {
+                            if (field.selection[option][0] == val) {
+                                val = field.selection[option][1];
+                                break;
+                            }
+                        }
+                    }
+                    if (val instanceof Array) {
+                        str.push('<span style="font-weight: bold">' + label + '</span>: ' +val[1]);
                     } else {
-                        str.push(label);
+                        span = '<span style="font-weight: bold">' + label + '</span>: ' +val;
+                         if (key == 'id') {
+                            oid = span;
+                        } else {
+                            str.push(span);
+                        }
                     }
                 }
             }
@@ -221,7 +234,7 @@ openerp.base_geoengine = function(openerp) {
                 },
                 eventListeners: {
                     "featureselected": function(event) {
-                        $("#map_info").html(formatHTML(event.feature.attributes));
+                        $("#map_info").html(formatHTML(event.feature.attributes, self.fields_view.fields));
                         $("#map_infobox").show();
                     },
                     "featureunselected": function() {
