017b751959cd1b0c9c6664bed163b8fcc3ab9f77
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-02-10 19:25:00 +0100

    Refactor WMTS to be able to add a generic config
    And extract Swisstopo to move it in a new module

--- base_geoengine/geo_view/geo_raster_layer.py
+++ base_geoengine/geo_view/geo_raster_layer.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Nicolas Bessi
-#    Copyright 2011-2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import fields, models
+# © 2011-2012 Nicolas Bessi (Camptocamp SA)
+# © 2016 Yannick Vaucher (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+
+
+class GeoRasterLayerType(models.Model):
+    _name = 'geoengine.raster.layer.type'
+
+    name = fields.Char(
+        translate=True, required=True
+    )
+    code = fields.Char(
+        required=True,
+    )
+    service = fields.Char(
+        required=True,
+    )
 
 
 class GeoRasterLayer(models.Model):
@@ -28,34 +26,68 @@ class GeoRasterLayer(models.Model):
         [('osm', 'OpenStreetMap'),
          ('mapbox', 'MapBox'),
          # FIXME ('google', 'Google'), see OCA/geospatial#63
+         ('wmts', 'WMTS'),
          ('d_wms', 'Distant WMS'),
-         ('swisstopo', 'swisstopo'),
-         ('openerp', 'OpenERP -- not implemented')],
+         ('odoo', 'Odoo field')],
         string="Raster layer type",
         default='osm',
         required=True)
     name = fields.Char(
         'Layer Name', size=256, translate=True, required=True)
     url = fields.Char('Service URL', size=1024)
-    google_type = fields.Selection(
-        [('G_NORMAL_MAP', 'Google normal map'),
-         ('G_SATELLITE_MAP', 'Google staellite map'),
-         ('G_HYBRID_MAP', 'Google Hybrid map'),
-         ('G_PHYSICAL_MAP', 'Google Physical map')],
-        string="Google raster layer type")
+
+    # technical field to display or not wmts options
+    is_wmts = fields.Boolean(compute='_get_is_wmts')
+    # wmts options
+    matrix_set = fields.Char("matrixSet")
+    format_suffix = fields.Char("formatSuffix", help="eg. png")
+    request_encoding = fields.Char("requestEncoding", help="eg. REST")
+    projection = fields.Char("projection", help="eg. EPSG:21781")
+    units = fields.Char(help="eg. m")
+    resolutions = fields.Char("resolutions")
+    max_extent = fields.Char("max_extent")
+    server_resolutions = fields.Char(
+        "resolutions",
+        help="List of dimensions separated by ',' eg. 50, 20, 10")
+    dimensions = fields.Char(
+        "dimensions",
+        help="List of dimensions separated by ','")
+    params = fields.Char(
+        "params",
+        help="Dictiorary of values for dimensions as JSON"
+    )
+
+    # technical field to display or not layer type
+    has_type = fields.Boolean(compute='_get_has_type')
+    type_id = fields.Many2one(
+        'geoengine.raster.layer.type', "Layer",
+        domain="[('service', '=', raster_type)]"
+    )
+    type = fields.Char(related='type_id.code')
     mapbox_id = fields.Char("Mapbox ID", size=256)
-    swisstopo_type = fields.Selection(
-        [('ch.swisstopo.pixelkarte-farbe', 'Color map'),
-         ('ch.swisstopo.swissimage', 'Aerial imagery')],
-        string="Swisstopo raster layer type")
-    swisstopo_time = fields.Char('Release date', size=256)
     sequence = fields.Integer('layer priority lower on top', default=6)
     overlay = fields.Boolean('Is overlay layer?')
     field_id = fields.Many2one(
-        'ir.model.fields', 'OpenERP layer field to use',
+        'ir.model.fields', 'Odoo layer field to use',
         domain=[('ttype', 'ilike', 'geo_'),
                 ('model', '=', 'view_id.model')])
     view_id = fields.Many2one(
         'ir.ui.view', 'Related View', domain=[('type', '=', 'geoengine')],
         required=True)
     use_to_edit = fields.Boolean('Use to edit')
+
+    @api.one
+    @api.depends('raster_type', 'is_wmts')
+    def _get_has_type(self):
+        self.has_type = self.raster_type in ('google', 'mapbox', 'is_wmts')
+
+    @api.one
+    @api.depends('raster_type')
+    def _get_is_wmts(self):
+        self.is_wmts = self.raster_type == 'wmts'
+
+    @api.one
+    @api.onchange('raster_type')
+    def onchange_set_wmts_options(self):
+        """ Abstract method for WMTS modules to set default options """
+        pass
