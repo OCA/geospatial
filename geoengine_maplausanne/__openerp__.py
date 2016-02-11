10db080221ad34d9a69a6f44622ad33630ff549a
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-02-11 19:06:17 +0100

    New module for WMTS map.lausanne.ch layer

---
+++ geoengine_maplausanne/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 Yannick Vaucher (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{'name': 'Geospatial - map.lausanne.ch layers',
+ 'version': '9.0.1.0.0',
+ 'category': 'GeoBI',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'maintainer': 'Camptocamp',
+ 'website': 'http://www.camptocamp.com/',
+ 'depends': ['base_geoengine'],
+ 'data': ['data/geo_raster_layer_type.xml'],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'application': True,
+ }
