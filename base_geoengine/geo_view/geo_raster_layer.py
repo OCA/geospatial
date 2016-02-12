f174ff5499d8b7056109e13737c55a973caceaa7
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-02-12 15:19:41 +0100

    By default use OSM

--- base_geoengine/geo_view/geo_raster_layer.py
+++ base_geoengine/geo_view/geo_raster_layer.py
@@ -25,13 +25,14 @@ class GeoRasterLayer(models.Model):
     _name = 'geoengine.raster.layer'
 
     raster_type = fields.Selection(
-        [('google', 'Google'),
-         ('osm', 'OpenStreetMap'),
+        [('osm', 'OpenStreetMap'),
          ('mapbox', 'MapBox'),
+         ('google', 'Google'),
          ('d_wms', 'Distant WMS'),
          ('swisstopo', 'swisstopo'),
          ('openerp', 'OpenERP -- not implemented')],
         string="Raster layer type",
+        default='osm',
         required=True)
     name = fields.Char(
         'Layer Name', size=256, translate=True, required=True)
