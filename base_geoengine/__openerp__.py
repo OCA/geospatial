f614cfbf7ce1f0c597ae6835a5757fbe73657ba9
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-07-29 15:36:22 +0200

    Bump version

--- base_geoengine/__openerp__.py
+++ base_geoengine/__openerp__.py
@@ -3,7 +3,7 @@
 # © 2016 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {'name': 'Geospatial support for Odoo',
- 'version': '9.0.1.2.2',
+ 'version': '9.0.1.2.5',
  'category': 'GeoBI',
  'author': "Camptocamp,ACSONE SA/NV,Odoo Community Association (OCA)",
  'license': 'AGPL-3',
