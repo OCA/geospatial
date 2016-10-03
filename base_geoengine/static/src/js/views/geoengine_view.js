e8736761c53dde5dc1ad9b8ffa9d17a7b8669e8d
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-10-03 12:17:48 +0200

    Remove disabling stopdown as it is included in OL3

--- base_geoengine/static/src/js/views/geoengine_view.js
+++ base_geoengine/static/src/js/views/geoengine_view.js
@@ -116,8 +116,6 @@ var GeoengineView = View.extend(geoengine_common.GeoengineMixin, {
             new OpenLayers.Control.SelectFeature(
                 {selectedFeatures:[]}, {})*/
         ];
-        this.selectFeatureControls[0].handlers.feature.stopDown = false;
-        this.selectFeatureControls[1].handlers.feature.stopDown = false;
     },
     load_view: function(context) {
         var self = this;
