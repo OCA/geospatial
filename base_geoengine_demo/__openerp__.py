6bb2a99f6a5aa54a0620bf0a47076b6a4d833b3c
--global <yannick.vaucher@camptocamp.com>
2016-10-08 21:31:28 +0200

    Move files to comply with module structure

--- base_geoengine_demo/__openerp__.py
+++ base_geoengine_demo/__openerp__.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Nicolas Bessi
-#    Copyright 2011-2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {'name': 'Geo spatial support Demo',
  'version': '9.0.1.0.0',
  'category': 'GeoBI',
@@ -28,8 +11,8 @@
      'base_geoengine'
  ],
  'data': [
-     'zip_view.xml',
-     'retail_machine_view.xml',
+     'views/zip.xml',
+     'views/retail_machine.xml',
      'data/retail_machine_geom.xml',
      'data/npa_geom.xml',
      'security/ir.model.access.csv'
