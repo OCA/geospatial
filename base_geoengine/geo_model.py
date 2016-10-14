66b6f098325fdf3afbbef68ab9713a6f374bfcc0
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-10-15 00:07:21 +0200

    Update license headers

--- base_geoengine/geo_model.py
+++ base_geoengine/geo_model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2011-2012 Nicolas Bessi (Camptocamp SA)
-# © 2016 Yannick Vaucher (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
+# Copyright 2016 Yannick Vaucher (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from openerp import models
 from openerp.exceptions import except_orm, MissingError
 from openerp.osv import fields
