d22e3c13905a27e45b0a9bf6a3668f6c44630f59
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-10-22 17:22:42 +0200

    [PORT] base_geoengine to 10.0 - remove old API

--- base_geoengine/fields.py
+++ base_geoengine/fields.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
+# Copyright 2016 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
+from operator import attrgetter
+
+from odoo import _
+from odoo.fields import Field
+
+from .geo_helper import geo_convertion_helper as convert
+
+logger = logging.getLogger(__name__)
 try:
     from shapely.geometry import Point
     from shapely.wkb import loads as wkbloads
     import geojson
 except ImportError:
-    logger = logging.getLogger(__name__)
     logger.warning('Shapely or geojson are not available in the sys path')
-
-from openerp.fields import Field
-
-from .geo_helper import geo_convertion_helper as convert
-from operator import attrgetter
 
 
 class GeoField(Field):
@@ -25,27 +28,140 @@ class GeoField(Field):
 
     geo_type = None
 
+    @property
+    def column_format(self):
+        return "ST_GeomFromText(%%s, %s)" % (self.srid,)
+
+    @property
+    def column_type(self):
+        return ('geometry', 'geometry')
+
     _slots = {
         'dim': 2,
         'srid': 900913,
         'gist_index': True,
     }
 
-    def convert_to_read(self, value, use_name_get=True):
-        res = convert.value_to_shape(value)
-        if res.is_empty:
-            return False
-        return geojson.dumps(res)
+    def convert_to_column(self, value, record):
+        """Convert value to database format
 
-    # properties used by to_column() to create a column instance
-    _column_dim = property(attrgetter('dim'))
-    _column_srid = property(attrgetter('srid'))
-    _column_gist_index = property(attrgetter('gist_index'))
+        value can be geojson, wkt, shapely geometry object.
+        If geo_direct_write in context you can pass diretly WKT"""
+        if not value:
+            return None
+        shape_to_write = self.entry_to_shape(value, same_type=True)
+        if shape_to_write.is_empty:
+            return None
+        else:
+            return shape_to_write.wkt
+
+    def convert_to_read(self, value, record, use_name_get=True):
+        # read hexadecimal value from database
+        shape = self.load_geo(value)
+        if not shape or shape.is_empty:
+            return False
+        return geojson.dumps(shape)
+
+    #
+    # Field description
+    #
+
+    # properties used by get_description()
+    _description_dim = property(attrgetter('dim'))
+    _description_srid = property(attrgetter('srid'))
+    _description_gist_index = property(attrgetter('gist_index'))
 
     @classmethod
     def load_geo(cls, wkb):
         """Load geometry into browse record after read was done"""
-        return wkbloads(wkb.decode('hex')) if wkb else False
+        return wkbloads(wkb, hex=True) if wkb else False
+
+    def create_geo_column(self, cr, col_name, table, model):
+        """Create a columns of type the geom"""
+        try:
+            cr.execute("SELECT AddGeometryColumn( %s, %s, %s, %s, %s)",
+                       (table,
+                        col_name,
+                        self.srid,
+                        self.geo_type,
+                        self.dim))
+            self._create_index(cr, table, col_name)
+        except Exception:
+            cr.rollback()
+            logger.exception('Cannot create column %s table %s:',
+                             col_name, table)
+            raise
+        finally:
+            cr.commit()
+
+        return True
+
+    def entry_to_shape(self, value, same_type=False):
+        """Transform input into an object"""
+        shape = convert.value_to_shape(value)
+        if same_type and not shape.is_empty:
+            if shape.geom_type.lower() != self.geo_type.lower():
+                msg = _('Geo Value %s must be of the same type %s as fields')
+                raise TypeError(msg % (shape.geom_type.lower(),
+                                       self.geo_type.lower()))
+        return shape
+
+    def _postgis_index_name(self, table, col_name):
+        return "%s_%s_gist_index" % (table, col_name)
+
+    def _create_index(self, cr, table, col_name):
+        if self.gist_index:
+            try:
+                cr.execute("CREATE INDEX %s ON %s USING GIST ( %s )" %
+                           (self._postgis_index_name(table, col_name),
+                            table,
+                            col_name))
+            except Exception:
+                cr.rollback()
+                logger.exception(
+                    'Cannot create gist index for col %s table %s:',
+                    col_name, table)
+                raise
+            finally:
+                cr.commit()
+
+    def update_geo_column(self, cr, col_name, table, model):
+        """Update the column type in the database.
+        """
+        query = ("""SELECT srid, type, coord_dimension
+                 FROM geometry_columns
+                 WHERE f_table_name = %s
+                 AND f_geometry_column = %s""")
+        cr.execute(query, (table, col_name))
+        check_data = cr.fetchone()
+        if not check_data:
+            raise TypeError(
+                "geometry_columns table seems to be corrupted. "
+                "SRID check is not possible")
+        if check_data[0] != self.srid:
+            raise TypeError(
+                "Reprojection of column is not implemented"
+                "We can not change srid %s to %s" % (
+                    self.srid, check_data[0]))
+        if check_data[1] != self.geo_type:
+            raise TypeError(
+                "Geo type modification is not implemented"
+                "We can not change type %s to %s" % (
+                    check_data[1], self.geo_type))
+        if check_data[2] != self.dim:
+            raise TypeError(
+                "Geo dimention modification is not implemented"
+                "We can not change dimention %s to %s" % (
+                    check_data[2], self.dim))
+        if self.gist_index:
+            cr.execute(
+                "SELECT indexname FROM pg_indexes WHERE indexname = %s",
+                (self._postgis_index_name(table, col_name),))
+            index = cr.fetchone()
+            if index:
+                return True
+            self._create_index(cr, table, col_name)
+        return True
 
 
 class GeoLine(GeoField):
