048a11c5e490c16146317b6ef34e00e2c789016d
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-02-17 15:54:54 +0100

    [MIG][9.0] geoengine_partner

--- geoengine_partner/__manifest__.py
+++ geoengine_partner/__manifest__.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Nicolas Bessi
-#    Copyright 2011-2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Geospatial support of partners',
- 'version': '8.0.0.1.0',
+ 'version': '9.0.1.0.0',
  'category': 'GeoBI',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'license': 'AGPL-3',
@@ -31,7 +14,6 @@
  'data': [
      'views/partner.xml'
  ],
- 'installable': False,
+ 'installable': True,
  'application': True,
- 'active': False,
  }
