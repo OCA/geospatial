6e1e1234406138eb32eb5010470286c658d17059
Pierre Verkest <pverkest@anybox.fr>
2017-04-05 16:47:48 +0200

    [10.0][MIG] geoengine_partner

--- geoengine_partner/__manifest__.py
+++ geoengine_partner/__manifest__.py
@@ -2,9 +2,9 @@
 # Copyright 2011-2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Geospatial support of partners',
- 'version': '9.0.1.0.0',
+ 'version': '10.0.1.0.0',
  'category': 'GeoBI',
- 'author': "Camptocamp,Odoo Community Association (OCA)",
+ 'author': "Camptocamp, Odoo Community Association (OCA)",
  'license': 'AGPL-3',
  'website': 'http://openerp.camptocamp.com',
  'depends': [
