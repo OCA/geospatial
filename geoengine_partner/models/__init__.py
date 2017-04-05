b2a8c7828bf50def4dc9c17d3577bb498f381885
Pierre Verkest <pverkest@anybox.fr>
2017-04-05 16:47:48 +0200

    [10.0][MIG] geoengine_partner

--- geoengine_partner/models/__init__.py
+++ geoengine_partner/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright 2011-2012 Nicolas Bessi <nicolas.bessi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
+from . import geo_partner
