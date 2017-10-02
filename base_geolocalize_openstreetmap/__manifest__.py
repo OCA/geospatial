24f3f4046b3cf8263d4b974cb76a9c6bca67ceb8
Benjamin Willig <benjamin.willig@acsone.eu>
2017-10-02 12:03:10 +0200

    [ADD] Module which only provides a method to call the Openstreetmap API

---
+++ base_geolocalize_openstreetmap/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Base Geolocalize Openstreetmap',
+    'summary': """
+        Open street map API call to geolocalize an address""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/geospatial',
+    'depends': [
+        'base_geoengine',
+    ],
+    'external_dependencies': {
+        'python': [
+            'requests'
+        ],
+    },
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ base_geolocalize_openstreetmap/models/geolocalize_openstreetmap.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import logging
+import requests
+
+from odoo import api, exceptions, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class GeoengineGeolocalizeOpenStreetmap(models.AbstractModel):
+
+    _name = 'geoengine.geolocalize.openstreetmap'
+
+    @api.model
+    def _geocode_address(
+            self, street, zip_code, city, state, country_name,
+            country_codes):
+        """Get the latitude and longitude by requesting "mapquestapi"
+        see http://open.mapquestapi.com/geocoding/
+        """
+        url = 'http://nominatim.openstreetmap.org/search'
+        pay_load = {
+            'limit': 1,
+            'format': 'json',
+            'street': street or '',
+            'postalCode': zip_code,
+            'city': city,
+            'state':  state,
+            'country': country_name,
+            'countryCodes': country_codes,
+        }
+
+        request_result = requests.get(url, params=pay_load)
+        try:
+            request_result.raise_for_status()
+        except Exception as e:
+            _logger.exception('Geocoding error')
+            raise exceptions.Warning(
+                _('Geocoding error. \n %s') % e.message)
+        values = request_result.json()
+        values = values and values[0] or {}
+        return values
