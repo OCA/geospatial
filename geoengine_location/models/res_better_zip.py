0131a7be7779c09dbd73b383639cc625eb370a72
Brett Wood <bwood@laslabs.com>
2017-10-25 14:05:40 -0700

    wip

--- geoengine_location/models/res_better_zip.py
+++ geoengine_location/models/res_better_zip.py
@@ -2,30 +2,27 @@
 # Copryight 2017 Laslabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, models, fields
 
-from odoo.addons.base_geoengine import fields
+from odoo.addons.base_geoengine import fields as geo_fields
 
 
-class BetterZip(models.Model):
+class ResBetterZip(models.Model):
 
     _inherit = 'res.better.zip'
 
-    geo_point = fields.GeoPoint(
+    geo_point = geo_fields.GeoPoint(
         string='UTM Coordinate Point',
-        compute='_compute_geo_point',
-        inverse='_inverse_geo_point',
-        store=True,
     )
 
-    @api.multi
-    @api.depends('latitude, longitude')
-    def _compute_geo_point(self):
-        for record in self:
-            record.geo_point = fields.GeoPoint.from_latlon(
-                self.env.cr, record.latitude, record.longitude,
-            )
+    # @api.multi
+    # @api.depends('latitude, longitude')
+    # def _compute_geo_point(self):
+    #     for record in self:
+    #         record.geo_point = fields.GeoPoint.from_latlon(
+    #             self.env.cr, record.latitude, record.longitude,
+    #         )
 
-    @api.multi
-    def _inverse_geo_point(self):
-        return
+    # @api.multi
+    # def _inverse_geo_point(self):
+    #     return
