1ad982dc01af16d746b7e6b5cbd4706167069d61
Emanuel Cino <ecino@compassion.ch>
2018-04-20 14:34:15 +0200

    [FIX] Replace openerp imports

--- base_geoengine/geo_view/geo_vector_symbol.py
+++ base_geoengine/geo_view/geo_vector_symbol.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import fields, models
+from odoo import fields, models
 
 
 class GeoVectorSymbol(models.Model):
