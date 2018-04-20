97e3ab0fac8b02df3f23d32122f3a3b39b81012e
Emanuel Cino <ecino@compassion.ch>
2018-04-20 14:34:15 +0200

    [FIX] Replace openerp imports

--- base_geoengine/geo_view/geo_vector_layer.py
+++ base_geoengine/geo_view/geo_vector_layer.py
@@ -2,7 +2,7 @@
 # Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
 # Copyright 2016 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import fields, models
+from odoo import fields, models
 
 SUPPORTED_ATT = ['float', 'integer', 'integer_big', 'related',
                  'function', 'date', 'datetime', 'char', 'text', 'selection']
