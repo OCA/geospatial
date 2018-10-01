5490861608102dde11bad2832f2eda435b020664
Tonow-c2c <thomas.nowicki@camptocamp.com>
2018-10-01 16:48:06 +0200

    [MIG] base_geoengine_demo: Migration to 11.0

--- base_geoengine_demo/__manifest__.py
+++ base_geoengine_demo/__manifest__.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {'name': 'Geo spatial support Demo',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'category': 'GeoBI',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'license': 'AGPL-3',
