01a1ae5c61e4c23c505532cc37f39070aedcdf10
Frederic Junod <frederic.junod@camptocamp.com>
2019-02-25 12:46:22 +0100

    Use EPSG:3857 instead of EPSG:900913
    EPSG:3857 is the official name for the spherical pseudo-mercator coordinate system.
    The unofficial 900913 code is deprecated and should not be used as the default anymore.

--- base_geoengine/geo_view/ir_view.py
+++ base_geoengine/geo_view/ir_view.py
@@ -29,7 +29,7 @@ class IrUIView(models.Model):
     vector_layer_ids = fields.One2many(
         'geoengine.vector.layer', 'view_id', 'Vector layers', required=True)
 
-    projection = fields.Char(default="EPSG:900913", required=True)
+    projection = fields.Char(default="EPSG:3857", required=True)
     default_extent = fields.Char(
         'Default map extent', size=128,
         default='-123164.85222423, 5574694.9538936, 1578017.6490538,'
