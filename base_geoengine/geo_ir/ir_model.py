0e4001128ae2e1d0808935fe8abf405eb0f2abde
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2019-05-10 11:04:08 +0200

    fixup! Fix selection of ttype field on ir.model.fields

--- base_geoengine/geo_ir/ir_model.py
+++ base_geoengine/geo_ir/ir_model.py
@@ -1,6 +1,6 @@
 # Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.addons import base
 if 'geoengine' not in base.models.ir_actions.VIEW_TYPES:
     base.models.ir_actions.VIEW_TYPES.append(('geoengine', 'Geoengine'))
