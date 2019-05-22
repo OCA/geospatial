a75d54443828f31547afffaf59b94e2d9b2dec0e
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2019-05-22 11:00:54 +0200

    Bump version

--- base_geoengine/__manifest__.py
+++ base_geoengine/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Geospatial support for Odoo',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'GeoBI',
     'author': "Camptocamp,ACSONE SA/NV,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
