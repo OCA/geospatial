3f3868725cb3a318791f9668c23a3c8cdc883806
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2019-10-01 09:36:31 +0200

    Fix geometry field creation missing configurations
    After changing the field in state `base` instead of `manual`.
    Those fields were created without configuration.
    srid would be 0 gemetry type in geometry_columns table would be
    GEOMETRY instead of POINT, LINESTRING, ...
    
    This fixes the creation of geometry to use AddGeometryColumn again.

--- base_geoengine/geo_db.py
+++ base_geoengine/geo_db.py
@@ -5,8 +5,10 @@ import logging
 
 from odoo import _
 from odoo.exceptions import MissingError
+from odoo.tools import sql
 
 logger = logging.getLogger("geoengine.sql")
+_schema = logging.getLogger("odoo.schema")
 
 
 def init_postgis(cr):
@@ -49,3 +51,42 @@ def init_postgis(cr):
                 "CREATE EXTENSION postgis_topology;\n"
             )
         )
+
+
+def create_geo_column(cr, tablename, columnname, geotype, srid, dim, comment=None):
+    """ Create a geometry column with the given type.
+
+    :params: srid: geometry's projection srid
+    :params: dim: geometry's dimension (2D or 3D)
+    """
+    cr.execute(
+        "SELECT AddGeometryColumn( %s, %s, %s, %s, %s)",
+        (tablename, columnname, srid, geotype, dim),
+    )
+    if comment:
+        # pylint: disable=E8103
+        cr.execute(
+            'COMMENT ON COLUMN "{}"."{}" IS %s'.format(tablename, columnname),
+            (comment,),
+        )
+    _schema.debug(
+        "Table %r: added geometry column %r of type %s", tablename, columnname, geotype
+    )
+
+
+def _postgis_index_name(table, col_name):
+    return "{}_{}_gist_index".format(table, col_name)
+
+
+def create_geo_index(cr, columnname, tablename):
+    """ Create the given index unless it exists. """
+    indexname = _postgis_index_name(tablename, columnname)
+    if sql.index_exists(cr, indexname):
+        return
+    # pylint: disable=E8103
+    cr.execute(
+        "CREATE INDEX {} ON {} USING GIST ( {} )".format(
+            indexname, tablename, columnname
+        )
+    )
+    _schema.debug("Table %r: created index %r", tablename, indexname)
