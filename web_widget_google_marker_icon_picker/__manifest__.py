4b4c81dcde3b2e297c095679be7e182dc2d4f84c
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-10-21 17:35:20 +0530

    [12.0][ADD] web_widget_google_marker_icon_picker

---
+++ web_widget_google_marker_icon_picker/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Google Marker Icon Picker',
+    'summary': "Google map widget allowing to set marker's color",
+    'version': '12.0.1.0.0',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/geospatial',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'depends': ['web_view_google_map'],
+    'data': [
+        'views/template.xml',
+    ],
+    'qweb': ['static/src/xml/marker_color.xml'],
+    'maintainers': [
+        'gityopie',
+        'wolfhall'
+    ],
+    'installable': True
+}
