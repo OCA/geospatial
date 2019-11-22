56d735548fb870237971bee759630ed43a95f5b0
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-11-22 16:24:55 +0530

    [IMP]web_widget_google_marker_icon_picker: Improved code.

--- web_widget_google_marker_icon_picker/static/src/js/view/map/map_renderer.js
+++ web_widget_google_marker_icon_picker/static/src/js/view/map/map_renderer.js
@@ -7,14 +7,8 @@ odoo.define('web_widget_google_marker_icon_picker.MapRenderer', function (requir
         _initLibraryProperties: function (params) {
             this._super.apply(this, arguments);
             if (this.mapLibrary === 'geometry') {
-                this.fieldMarkerColor = params.fieldMarkerColor;
                 this.iconUrl = '/web_widget_google_marker_icon_picker/static/src/img/markers/';
             }
         },
-        _createMarker: function (latLng, record, color) {
-            var color = (record.data[this.fieldMarkerColor] ? record.data[this.fieldMarkerColor] : color) || 'red';
-            this._super(latLng, record, color);
-        },
     });
-
 });
