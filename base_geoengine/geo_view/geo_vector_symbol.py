3f6dd53293f6e90dfedb35c26108290dd0cb5d4f
Timon Tschanz <timon.tschanz@camptocamp.com>
2020-01-14 13:21:32 +0100

    pre-commit with correct config

--- base_geoengine/geo_view/geo_vector_symbol.py
+++ base_geoengine/geo_view/geo_vector_symbol.py
@@ -4,17 +4,15 @@ from odoo import fields, models
 
 
 class GeoVectorSymbol(models.Model):
-    _name = 'geoengine.vector.symbol'
+    _name = "geoengine.vector.symbol"
     _description = "Vector Layer Symbol"
 
-    vector_layer_id = fields.Many2one('geoengine.vector.layer')
+    vector_layer_id = fields.Many2one("geoengine.vector.layer")
     fieldname = fields.Char(
         "Category field",
         help="Name of the char field or selection field used for comparison",
     )
-    value = fields.Char(
-        help="All object equal to this value will use this symbol"
-    )
+    value = fields.Char(help="All object equal to this value will use this symbol")
     img = fields.Char(
         help="URL of the image to use. You can put an image in your module "
         "in static folder e.g. 'base_geoengine/static/img/map-marker.png'"
