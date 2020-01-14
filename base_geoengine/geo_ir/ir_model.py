eac47f51ec2cc17ed70d2e53683aedb2e2b8cebe
Timon Tschanz <timon.tschanz@camptocamp.com>
2020-01-14 13:21:32 +0100

    pre-commit with correct config

--- base_geoengine/geo_ir/ir_model.py
+++ base_geoengine/geo_ir/ir_model.py
@@ -4,35 +4,35 @@ from odoo import fields, models
 
 from odoo.addons import base
 
-if 'geoengine' not in base.models.ir_actions.VIEW_TYPES:
-    base.models.ir_actions.VIEW_TYPES.append(('geoengine', 'Geoengine'))
+if "geoengine" not in base.models.ir_actions.VIEW_TYPES:
+    base.models.ir_actions.VIEW_TYPES.append(("geoengine", "Geoengine"))
 
 GEO_TYPES = [
-    ('geo_polygon', 'geo_polygon'),
-    ('geo_multi_polygon', 'geo_multi_polygon'),
-    ('geo_point', 'geo_point'),
-    ('geo_multi_point', 'geo_multi_point'),
-    ('geo_line', 'geo_line'),
-    ('geo_multi_line', 'geo_multi_line'),
+    ("geo_polygon", "geo_polygon"),
+    ("geo_multi_polygon", "geo_multi_polygon"),
+    ("geo_point", "geo_point"),
+    ("geo_multi_point", "geo_multi_point"),
+    ("geo_line", "geo_line"),
+    ("geo_multi_line", "geo_multi_line"),
 ]
 
 POSTGIS_GEO_TYPES = [
-    ('POINT', 'POINT'),
-    ('MULTIPOINT', 'MULTIPOINT'),
-    ('LINESTRING', 'LINESTRING'),
-    ('MULTILINESTRING', 'MULTILINESTRING'),
-    ('POLYGON', 'POLYGON'),
-    ('MULTIPOLYGON', 'MULTIPOLYGON'),
+    ("POINT", "POINT"),
+    ("MULTIPOINT", "MULTIPOINT"),
+    ("LINESTRING", "LINESTRING"),
+    ("MULTILINESTRING", "MULTILINESTRING"),
+    ("POLYGON", "POLYGON"),
+    ("MULTIPOLYGON", "MULTIPOLYGON"),
 ]
 
 
 class IrModelField(models.Model):
-    _inherit = 'ir.model.fields'
+    _inherit = "ir.model.fields"
 
-    srid = fields.Integer('srid', required=False)
+    srid = fields.Integer("srid", required=False)
     geo_type = fields.Selection(POSTGIS_GEO_TYPES, string="PostGIs type")
     dim = fields.Selection(
-        [('2', '2'), ('3', '3'), ('4', '4')], string="PostGIs Dimension"
+        [("2", "2"), ("3", "3"), ("4", "4")], string="PostGIs Dimension"
     )
-    gist_index = fields.Boolean('Create gist index')
+    gist_index = fields.Boolean("Create gist index")
     ttype = fields.Selection(selection_add=GEO_TYPES)
