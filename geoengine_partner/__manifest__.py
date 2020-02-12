c2b05bf2546916b9bacd23835bcb8b007f1cfe98
Josue Bulle <17401810+josueBulle@users.noreply.github.com>
2020-02-12 16:37:10 +0100

    [MIG] geoengine_partner: Migration to 11.0

--- geoengine_partner/__manifest__.py
+++ geoengine_partner/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2011-2017 Camptocamp SA
+# Copyright 2011-2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Geospatial support of partners',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'category': 'GeoBI',
  'author': "Camptocamp, Odoo Community Association (OCA)",
  'license': 'AGPL-3',
- 'website': 'http://openerp.camptocamp.com',
+ 'website': 'https://github.com/OCA/geospatial',
  'depends': [
      'base',
      'base_geoengine'
