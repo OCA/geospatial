25b4b3f4ec596665b5aabfb025af20546a318582
Josue Bulle <17401810+josueBulle@users.noreply.github.com>
2020-03-02 14:33:11 +0100

    [MIG] geoengine_partner: Migration to 12.0

--- geoengine_partner/__manifest__.py
+++ geoengine_partner/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2011-2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Geospatial support of partners',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'category': 'GeoBI',
  'author': "Camptocamp, Odoo Community Association (OCA)",
  'license': 'AGPL-3',
--- geoengine_partner/models/partner.py
+++ geoengine_partner/models/partner.py
@@ -1,12 +1,11 @@
 # Copyright 2011-2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.base_geoengine import geo_model
-from odoo.addons.base_geoengine import fields
+from odoo import fields, models
 
 
-class ResPartner(geo_model.GeoModel):
-    """Add geo_point to partner using a function filed"""
+class ResPartner(models.Model):
+    """Add geo_point to partner using a function field"""
     _inherit = "res.partner"
 
     geo_point = fields.GeoPoint("Address coordinates")
