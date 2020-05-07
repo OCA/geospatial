adfdde0b40906ea2375c8bcd8a86c3578b1f771f
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-05-07 16:10:45 +0200

    [MIG] geoengine_swisstopo: Migration to 13.0

--- geoengine_swisstopo/__manifest__.py
+++ geoengine_swisstopo/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "GeoEngine - Swisstopo layers",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "GeoBI",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
---
+++ geoengine_swisstopo/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds Swisstopo raster layer support.
+The api is free to use for some layers only and with some restrictions.
+For more information see the `official documentation <https://api3.geo.admin.ch/index.html>`_
