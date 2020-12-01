e51ee836fcd3800bc9b21d17f4538b75075ef6e7
nans <nans.lefebvre@acsone.eu>
2020-12-01 14:59:27 +0100

    [MIG] base_geolocalize_openstreetmap: Migration to 12.0

--- base_geolocalize_openstreetmap/__manifest__.py
+++ base_geolocalize_openstreetmap/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,7 @@
     "name": "Base Geolocalize Openstreetmap",
     "summary": """
         Open street map API call to geolocalize an address""",
-    "version": "10.0.1.0.1",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/geospatial",
