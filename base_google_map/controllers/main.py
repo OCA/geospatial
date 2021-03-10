06c2b210218f11068d5201ef58de3e234f201721
Brian McMaster <brian@mcmpest.com>
2021-03-10 13:00:58 -0500

    [IMP] base_google_map: black, isort, prettier

--- base_google_map/controllers/main.py
+++ base_google_map/controllers/main.py
@@ -5,10 +5,9 @@ from odoo import http
 
 
 class Main(http.Controller):
-
-    @http.route('/web/map_theme', type='json', auth='user')
+    @http.route("/web/map_theme", type="json", auth="user")
     def map_theme(self):
-        ICP = http.request.env['ir.config_parameter'].sudo()
-        theme = ICP.get_param('google.maps_theme', default='default')
-        res = {'theme': theme}
+        ICP = http.request.env["ir.config_parameter"].sudo()
+        theme = ICP.get_param("google.maps_theme", default="default")
+        res = {"theme": theme}
         return res
