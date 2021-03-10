65718f75eec052eb0fb27cf98fc052c545db50c4
Brian McMaster <brian@mcmpest.com>
2021-03-10 14:06:18 -0500

    [MIG] web_view_google_map: Migration to 13.0

--- web_view_google_map/hooks.py
+++ web_view_google_map/hooks.py
@@ -5,12 +5,12 @@
 def uninstall_hook(cr, registry):
     cr.execute(
         "UPDATE ir_act_window "
-        "SET view_mode=replace(view_mode, ',map', '')"
-        "WHERE view_mode LIKE '%,map%';"
+        "SET view_mode=replace(view_mode, ',google_map', '')"
+        "WHERE view_mode LIKE '%,google_map%';"
     )
     cr.execute(
         "UPDATE ir_act_window "
-        "SET view_mode=replace(view_mode, 'map,', '')"
-        "WHERE view_mode LIKE '%map,%';"
+        "SET view_mode=replace(view_mode, 'google_map,', '')"
+        "WHERE view_mode LIKE '%google_map,%';"
     )
-    cr.execute("DELETE FROM ir_act_window " "WHERE view_mode = 'map';")
+    cr.execute("DELETE FROM ir_act_window WHERE view_mode = 'google_map';")
