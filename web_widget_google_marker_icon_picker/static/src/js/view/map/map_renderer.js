2ab50702cf9b3b5122b4b2c43ca73f1b4bb33b34
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2021-04-08 11:16:14 +0530

    [FIX] Fixed a travis.

--- web_widget_google_marker_icon_picker/static/src/js/view/map/map_renderer.js
+++ web_widget_google_marker_icon_picker/static/src/js/view/map/map_renderer.js
@@ -1,13 +1,14 @@
-odoo.define('web_widget_google_marker_icon_picker.MapRenderer', function (require) {
-    'use strict';
+odoo.define("web_widget_google_marker_icon_picker.MapRenderer", function (require) {
+    "use strict";
 
-    var MapRenderer = require('web_view_google_map.MapRenderer');
+    var MapRenderer = require("web_view_google_map.MapRenderer");
 
     MapRenderer.include({
         _initLibraryProperties: function (params) {
-            this._super.apply(this, arguments);
-            if (this.mapLibrary === 'geometry') {
-                this.iconUrl = '/web_widget_google_marker_icon_picker/static/src/img/markers/';
+            this._super.apply(params);
+            if (this.mapLibrary === "geometry") {
+                this.iconUrl =
+                    "/web_widget_google_marker_icon_picker/static/src/img/markers/";
             }
         },
     });
--- web_widget_google_marker_icon_picker/static/src/js/widget/field_marker.js
+++ web_widget_google_marker_icon_picker/static/src/js/widget/field_marker.js
@@ -1,27 +1,29 @@
-odoo.define('web_widget_google_marker_icon_picker.MarkerColor', function (require) {
-    'use strict';
+odoo.define("web_widget_google_marker_icon_picker.MarkerColor", function (require) {
+    "use strict";
 
-    var core = require('web.core');
-    var AbstractField = require('web.AbstractField');
-    var registry = require('web.field_registry');
+    var core = require("web.core");
+    var AbstractField = require("web.AbstractField");
+    var registry = require("web.field_registry");
     var qweb = core.qweb;
 
     var MarkerColorPicker = AbstractField.extend({
-        tag_template: 'FieldMarkerColorPicker',
-        className: 'o_field_char_marker_color',
-        supportedFieldTypes: ['char'],
+        tag_template: "FieldMarkerColorPicker",
+        className: "o_field_char_marker_color",
+        supportedFieldTypes: ["char"],
         events: _.extend({}, AbstractField.prototype.events, {
-            'click .dropdown-toggle': '_onOpenMarkerPicker'
+            "click .dropdown-toggle": "_onOpenMarkerPicker",
         }),
 
         _onOpenMarkerPicker: function (ev) {
             ev.preventDefault();
-            this.$marker_picker = $(qweb.render('FieldMarkerColorPicker.marker_picker', {
-                'widget': this,
-            }));
+            this.$marker_picker = $(
+                qweb.render("FieldMarkerColorPicker.marker_picker", {
+                    widget: this,
+                })
+            );
             $(ev.currentTarget).after(this.$marker_picker);
             this.$marker_picker.dropdown();
-            this.$marker_picker.one('click', 'a', this._onSelectMarker.bind(this));
+            this.$marker_picker.one("click", "a", this._onSelectMarker.bind(this));
         },
 
         _renderEdit: function () {
@@ -33,22 +35,21 @@ odoo.define('web_widget_google_marker_icon_picker.MarkerColor', function (requir
         },
 
         _renderMarker: function () {
-            this.$el.html(qweb.render(this.tag_template, { color: this.value }));
+            this.$el.html(qweb.render(this.tag_template, {color: this.value}));
         },
 
         _onSelectMarker: function (ev) {
             ev.preventDefault();
-            var color = $(ev.currentTarget).attr('data-color');
+            var color = $(ev.currentTarget).attr("data-color");
             var self = this;
-            this.trigger_up('field_changed', {
+            this.trigger_up("field_changed", {
                 dataPointID: self.dataPointID,
                 changes: {
-                    [self.name]: color
+                    [self.name]: color,
                 },
             });
-        }
+        },
     });
 
-    registry.add('google_marker_picker', MarkerColorPicker);
-
+    registry.add("google_marker_picker", MarkerColorPicker);
 });
