aa707a424bd3856fb359b6035e0e4e053f350dbe
Christopher Ormaza <chris.ormaza@gmail.com>
2022-03-29 17:27:24 -0500

    [13.0][MIG] base_google_map

--- base_google_map/__manifest__.py
+++ base_google_map/__manifest__.py
@@ -2,26 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Google Maps Integration',
-    'summary': 'View modes and widgets to integrate Google Maps in your UI',
-    'version': '12.0.1.0.0',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/geospatial',
-    'license': 'AGPL-3',
-    'category': 'Extra Tools',
-    'depends': [
-        'base_setup',
-        'base_geolocalize',
+    "name": "Google Maps Integration",
+    "summary": "View modes and widgets to integrate Google Maps in your UI",
+    "version": "13.0.1.0.0",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/geospatial",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        "base_setup",
+        "base_geolocalize",
     ],
-    'data': [
-        'data/google_maps_libraries.xml',
-        'views/res_config_settings.xml'
-    ],
-    'images': ['static/description/thumbnails.png'],
-    'installable': True,
-    'application': True,
-    'maintainers': [
-        'gityopie',
-        'wolfhall'
-    ],
+    "data": ["data/google_maps_libraries.xml", "views/res_config_settings.xml"],
+    "images": ["static/description/thumbnails.png"],
+    "installable": True,
+    "application": True,
+    "maintainers": ["gityopie", "wolfhall"],
 }
