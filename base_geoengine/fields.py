390c3b5299406e8df4a7d651151f9983780de725
Sergio Sancho <sersanchus@gmail.com>
2022-06-10 14:40:40 +0200

    Fix name and order of parameters of the gist index creation function

--- base_geoengine/fields.py
+++ base_geoengine/fields.py
@@ -7,7 +7,7 @@ from operator import attrgetter
 from odoo import _, fields
 from odoo.tools import sql
 
-from .geo_db import create_geo_column
+from .geo_db import create_geo_column, create_geo_index
 from .geo_helper import geo_convertion_helper as convert
 
 logger = logging.getLogger(__name__)
@@ -138,7 +138,7 @@ class GeoField(fields.Field):
             index = cr.fetchone()
             if index:
                 return True
-            self._create_index(cr, model._table, self.name)
+            create_geo_index(cr, model._table, self.name)
         return True
 
     def update_db_column(self, model, column):
