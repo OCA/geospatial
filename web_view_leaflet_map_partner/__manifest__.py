6c81d6b9a7c1298f22f3ebe72c922b355f048e3c
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-09-19 13:08:46 +0200

    [ADD] web_view_leaflet_map_partner

---
+++ web_view_leaflet_map_partner/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Leaflet Map View for Partners (OpenStreetMap)",
+    "summary": "TODO",
+    "version": "12.0.1.0.1",
+    "development_status": "Alpha",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/geospatial",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        "web_view_leaflet_map",
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "demo": [
+        "demo/res_partner.xml",
+    ],
+    "installable": True,
+    "maintainers": [
+        "legalsylvain",
+    ],
+}
