995706978da2672a263c45ce1a1b9bd757d34ccc
Yannick Payot <yannick.payot@camptocamp.com>
2023-01-31 17:47:56 +0100

    [MIG] base_geoengine: Migration to 14.0

--- base_geoengine/geo_view/ir_view.py
+++ base_geoengine/geo_view/ir_view.py
@@ -1,5 +1,5 @@
 # Copyright 2011-2012 Nicolas Bessi (Camptocamp SA)
-# Copyright 2016 Yannick Vaucher (Camptocamp SA)
+# Copyright 2016-2023 Yannick Payot (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
@@ -7,7 +7,10 @@ from odoo import fields, models
 class IrUIView(models.Model):
     _inherit = "ir.ui.view"
 
-    type = fields.Selection(selection_add=[("geoengine", "GeoEngine")])
+    type = fields.Selection(
+        selection_add=[("geoengine", "GeoEngine")],
+        ondelete={"geoengine": "cascade"},
+    )
 
     raster_layer_ids = fields.One2many(
         "geoengine.raster.layer", "view_id", "Raster layers", required=False
@@ -20,7 +23,6 @@ class IrUIView(models.Model):
     projection = fields.Char(default="EPSG:3857", required=True)
     default_extent = fields.Char(
         "Default map extent",
-        size=128,
         default="-123164.85222423, 5574694.9538936, 1578017.6490538,"
         " 6186191.1800898",
     )
