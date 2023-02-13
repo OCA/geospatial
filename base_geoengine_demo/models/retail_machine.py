c34aee7caf380453149f54f02fae2af783b6e1ae
Samuel Kouff <samuel.kouff@hotmail.com>
2023-02-13 14:33:15 +0100

    [IMP] base_geoengine_demo: black, isort, prettier

--- base_geoengine_demo/models/retail_machine.py
+++ base_geoengine_demo/models/retail_machine.py
@@ -10,27 +10,23 @@ class RetailMachine(models.Model):
     _name = "geoengine.demo.automatic.retailing.machine"
     _description = "Geoengine demo retailing machine"
 
-    the_point = fields.GeoPoint('Coordinate')
-    the_line = fields.GeoLine('Power supply line', index=True)
-    total_sales = fields.Float('Total sale', index=True)
-    money_level = fields.Char('Money level', size=32, index=True)
-    state = fields.Selection([('hs', 'HS'),
-                              ('ok', 'OK')],
-                             'State',
-                             index=True)
-    name = fields.Char('Serial number', size=64, required=True)
-    zip_id = fields.Many2one('dummy.zip')
+    the_point = fields.GeoPoint("Coordinate")
+    the_line = fields.GeoLine("Power supply line", index=True)
+    total_sales = fields.Float("Total sale", index=True)
+    money_level = fields.Char("Money level", size=32, index=True)
+    state = fields.Selection([("hs", "HS"), ("ok", "OK")], "State", index=True)
+    name = fields.Char("Serial number", size=64, required=True)
+    zip_id = fields.Many2one("dummy.zip")
 
-    @api.onchange('the_point')
+    @api.onchange("the_point")
     def onchange_geo_point(self):
-        """ Exemple of on change on the point
+        """Exemple of on change on the point
         Lookup in zips if the code is within an area.
         Change the zip_id field accordingly
         """
         if self.the_point:
-            zip_match = self.env['dummy.zip'].geo_search(
-                geo_domain=[('the_geom', 'geo_contains', self.the_point)],
-                limit=1
+            zip_match = self.env["dummy.zip"].geo_search(
+                geo_domain=[("the_geom", "geo_contains", self.the_point)], limit=1
             )
             if zip_match:
                 self.zip_id = zip_match[0]
