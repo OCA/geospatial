b131d16d083d56a7ef0ea76888b202af66a92fb7
Samuel Kouff <samuel.kouff@hotmail.com>
2023-02-18 13:18:42 +0100

    [FIX] base_geoengine: fix geoengine view renderer

--- base_geoengine/static/src/js/views/geoengine/geoengine_arch_parser.esm.js
+++ base_geoengine/static/src/js/views/geoengine/geoengine_arch_parser.esm.js
@@ -2,13 +2,4 @@
 
 import {XMLParser} from "@web/core/utils/xml";
 
-export class GeoengineArchParser extends XMLParser {
-    parse(arch) {
-        const xmlDoc = this.parseXML(arch);
-        const the_geom_field = xmlDoc.getAttribute("the_geom");
-        //const limit = xmlDoc.getAttribute("limit") || 80;
-        return {
-            the_geom_field,
-        };
-    }
-}
+export class GeoengineArchParser extends XMLParser {}
--- base_geoengine/static/src/js/views/geoengine/geoengine_renderer.esm.js
+++ base_geoengine/static/src/js/views/geoengine/geoengine_renderer.esm.js
@@ -7,7 +7,6 @@ const {Component, onWillStart, onMounted, onRendered} = owl;
 export class GeoengineRenderer extends Component {
     setup() {
         super.setup();
-
         this.orm = useService("orm");
 
         onWillStart(async () => {
@@ -15,13 +14,100 @@ export class GeoengineRenderer extends Component {
         });
 
         onMounted(async () => {
-            // This.createLayers();
             this.renderMap();
+        });
+
+        onRendered(() => {
             this.renderVectorLayers();
         });
     }
 
-    renderVectorLayers() {}
+    createVectorLayer() {
+        this.features = new ol.Collection();
+        this.source = new ol.source.Vector({features: this.features});
+        return new ol.layer.Vector({
+            source: this.source,
+            style: new ol.style.Style({
+                fill: new ol.style.Fill({
+                    color: "#ee9900",
+                    opacity: 0.1,
+                }),
+                stroke: new ol.style.Stroke({
+                    color: "#333333",
+                    width: 2,
+                }),
+                image: new ol.style.Circle({
+                    radius: 7,
+                    fill: new ol.style.Fill({
+                        color: "#ffcc33",
+                    }),
+                }),
+            }),
+        });
+    }
+
+    addFeatures() {
+        this.props.data.records.forEach((record) => {
+            const value = record._values;
+            const ft = new ol.Feature({
+                geometry: new ol.format.GeoJSON().readGeometry(value.the_geom),
+                labelPoint: new ol.format.GeoJSON().readGeometry(value.the_geom),
+            });
+            this.source.addFeature(ft);
+        });
+    }
+
+    updateMapZoom(zoom) {
+        if (this.source) {
+            var extent = this.source.getExtent();
+            var infinite_extent = [Infinity, Infinity, -Infinity, -Infinity];
+            if (extent !== infinite_extent) {
+                var map_view = this.map.getView();
+                if (map_view) {
+                    map_view.fit(extent, {maxZoom: zoom});
+                }
+            }
+        }
+    }
+
+    renderVectorLayers() {
+        if (this.source) {
+            this.source.clear();
+        } else {
+            this.vectorLayer = this.createVectorLayer();
+        }
+        this.addFeatures();
+        if (this.map !== undefined) {
+            this.updateMapZoom(15);
+        }
+    }
+
+    onHover() {
+        const selectStyle = new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: "#eeeeee",
+            }),
+            stroke: new ol.style.Stroke({
+                color: "rgba(255, 255, 255, 0.8)",
+                width: 2,
+            }),
+        });
+        let selected = null;
+        this.map.on("pointermove", (e) => {
+            if (selected !== null) {
+                selected.setStyle(undefined);
+                selected = null;
+            }
+            this.map.forEachFeatureAtPixel(e.pixel, (f) => {
+                selected = f;
+                selectStyle
+                    .getFill()
+                    .setColor(f.get("COLOR") || "rgba(255, 255, 255, 0.4)");
+                f.setStyle(selectStyle);
+                return true;
+            });
+        });
+    }
 
     renderMap() {
         if (!this.map) {
@@ -37,19 +123,14 @@ export class GeoengineRenderer extends Component {
                     zoom: 2,
                 }),
             });
+            this.map.addLayer(this.vectorLayer);
+            this.updateMapZoom(10);
             this.setupControls();
+            this.onHover();
         }
     }
 
     setupControls() {
-        /* This.zoomToExtent = new ol.control.ZoomToExtent({
-                extent: [
-                    813079.7791264898, 5929220.284081122, 848966.9639063801,
-                    5936863.986909639,
-                ],
-            });
-
-            this.map.addControl(zoomToExtent)*/
         const fullscreen = new ol.control.FullScreen();
         const scaleLine = new ol.control.ScaleLine();
         this.map.addControl(fullscreen);
