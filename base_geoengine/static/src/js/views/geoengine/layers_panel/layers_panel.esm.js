d7e5157e6710d39993297be3e39818860e8d3403
Samuel Kouff <samuel.kouff@hotmail.com>
2023-03-02 08:54:02 +0100

    [FIX] base_geoengine: code cleaning

--- base_geoengine/static/src/js/views/geoengine/layers_panel/layers_panel.esm.js
+++ base_geoengine/static/src/js/views/geoengine/layers_panel/layers_panel.esm.js
@@ -7,8 +7,12 @@ const {Component, onWillStart} = owl;
 export class LayersPanel extends Component {
     setup() {
         super.setup();
+
         this.orm = useService("orm");
 
+        /**
+         * Call the model method "get_geoengine_layers" to get all the layers in the database and add them to the store.
+         */
         onWillStart(async () => {
             const result = await this.orm.call(
                 this.props.model,
@@ -16,11 +20,18 @@ export class LayersPanel extends Component {
                 []
             );
             this.geoengine_layers = result;
+
+            // Set layers in the store
             store.setRasters(this.geoengine_layers.backgrounds);
             store.setVectors(this.geoengine_layers.actives);
         });
     }
 
+    /**
+     * Is called when a raster layer is changed. The raster layer is set to visible and then
+     * the method notifies the store of the change.
+     * @param {*} layer
+     */
     onRasterChange(layer) {
         const indexRaster = store
             .getRasters()
@@ -36,7 +47,13 @@ export class LayersPanel extends Component {
         store.onRasterLayerChanged(newRasters);
     }
 
+    /**
+     * Is called when a vector layer is changed. The vector layer is set to visible and then
+     * the method notifies the store of the change.
+     * @param {*} layer
+     */
     onVectorChange(layer) {
+        console.log("je passe");
         const indexVector = store
             .getVectors()
             .findIndex((vector) => vector.name === layer.name);
@@ -49,6 +66,11 @@ export class LayersPanel extends Component {
         store.onVectorLayerChanged(newVectors);
     }
 
+    /**
+     * Returns wether the layer is visible or not.
+     * @param {*} layer
+     * @returns
+     */
     getVisibleLayer(layer) {
         return layer.isVisible;
     }
