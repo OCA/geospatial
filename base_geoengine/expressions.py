8a49a7caa8d4cbbf3618b99e3d38778f514f2ed0
Samuel Kouff <samuel.kouff@hotmail.com>
2023-03-10 11:27:20 +0100

    [FIX] base_geoengine: fix __leaf_to_sql by adding our own where_calc method with our alias to perform search method and fix tests

--- base_geoengine/expressions.py
+++ base_geoengine/expressions.py
@@ -1,8 +1,12 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import random
+import string
+
 from odoo.osv import expression
 from odoo.osv.expression import TERM_OPERATORS
+from odoo.tools import Query
 
 from .fields import GeoField
 from .geo_operators import GeoOperator
@@ -45,18 +49,34 @@ def __leaf_to_sql(self, leaf, model, alias):
                     rel_model = model.env[rel_model]
                     # we compute the attributes search on spatial rel
                     if ref_search[key]:
-                        rel_query = rel_model._where_calc(
-                            ref_search[key], active_test=True
+                        rel_alias = (
+                            rel_model._table
+                            + "_"
+                            + "".join(random.choices(string.ascii_lowercase, k=5))
+                        )
+                        rel_query = where_calc(
+                            rel_model,
+                            ref_search[key],
+                            active_test=True,
+                            alias=rel_alias,
                         )
                         model._apply_ir_rules(rel_query, "read")
-                        if len(GEO_OPERATORS[operator]) > 1:
+                        if operator == "geo_equal":
                             rel_query.add_where(
-                                f'{GEO_OPERATORS[operator]}("{alias}"."{left}", {rel_col})'
+                                f'"{alias}"."{left}" {GEO_OPERATORS[operator]} '
+                                f"{rel_alias}.{rel_col}"
+                            )
+                        elif operator in ("geo_greater", "geo_lesser"):
+                            rel_query.add_where(
+                                f"ST_Area({alias}.{left}) {GEO_OPERATORS[operator]} "
+                                f"ST_Area({rel_alias}.{rel_col})"
                             )
                         else:
                             rel_query.add_where(
-                                f'"{alias}"."{left}" {GEO_OPERATORS[operator]} {rel_col}'
+                                f'{GEO_OPERATORS[operator]}("{alias}"."{left}", '
+                                f"{rel_alias}.{rel_col})"
                             )
+
                         subquery, subparams = rel_query.subselect("1")
                         sub_queries.append(f"EXISTS({subquery})")
                         params += subparams
@@ -69,38 +89,42 @@ def __leaf_to_sql(self, leaf, model, alias):
         return original__leaf_to_sql(self, leaf=leaf, model=model, alias=alias)
 
 
-def get_geo_func(
-    current_operator, operator, left, right, params, table, rel_col=None, rel_model=None
-):
+def get_geo_func(current_operator, operator, left, right, params, table):
     match operator:
         case "geo_greater":
-            query = current_operator.get_geo_greater_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_greater_sql(table, left, right, params)
         case "geo_lesser":
-            query = current_operator.get_geo_lesser_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_lesser_sql(table, left, right, params)
         case "geo_equal":
-            query = current_operator.get_geo_equal_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_equal_sql(table, left, right, params)
         case "geo_touch":
-            query = current_operator.get_geo_touch_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_touch_sql(table, left, right, params)
         case "geo_within":
-            query = current_operator.get_geo_within_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_within_sql(table, left, right, params)
         case "geo_contains":
-            query = current_operator.get_geo_contains_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_contains_sql(table, left, right, params)
         case "geo_intersect":
-            query = current_operator.get_geo_intersect_sql(
-                table, left, right, params, rel_col=rel_col, rel_model=rel_model
-            )
+            query = current_operator.get_geo_intersect_sql(table, left, right, params)
+        case _:
+            raise NotImplementedError(f"The operator {operator} is not supported")
+    return query
+
+
+def where_calc(model, domain, active_test=True, alias=None):
+    """
+    This method is copied from base, we need to create our own query.
+    """
+    # if the object has an active field ('active', 'x_active'), filter out all
+    # inactive records unless they were explicitly asked for
+    if model._active_name and active_test and model._context.get("active_test", True):
+        # the item[0] trick below works for domain items and '&'/'|'/'!'
+        # operators too
+        if not any(item[0] == model._active_name for item in domain):
+            domain = [(model._active_name, "=", 1)] + domain
+
+    query = Query(model.env.cr, alias, model._table)
+    if domain:
+        return expression.expression(domain, model, alias=alias, query=query).query
     return query
 
 
