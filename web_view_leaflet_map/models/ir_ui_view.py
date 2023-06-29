dbddba493333c04431232b5453a9a16defdcae10
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-29 14:33:56 +0200

    [IMP] web_view_leaflet_map: black, isort, prettier

--- web_view_leaflet_map/models/ir_ui_view.py
+++ web_view_leaflet_map/models/ir_ui_view.py
@@ -7,6 +7,6 @@ from odoo import fields, models
 
 
 class IrUiView(models.Model):
-    _inherit = 'ir.ui.view'
+    _inherit = "ir.ui.view"
 
-    type = fields.Selection(selection_add=[('leaflet_map', 'Leaflet Map')])
+    type = fields.Selection(selection_add=[("leaflet_map", "Leaflet Map")])
