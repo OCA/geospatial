b614a23661f5c124115e8337bc6a9f9fceeac05e
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-29 14:33:56 +0200

    [IMP] web_view_leaflet_map: black, isort, prettier

--- web_view_leaflet_map/models/ir_http.py
+++ web_view_leaflet_map/models/ir_http.py
@@ -6,13 +6,15 @@ from odoo import models
 
 
 class Http(models.AbstractModel):
-    _inherit = 'ir.http'
+    _inherit = "ir.http"
 
     def session_info(self):
         result = super(Http, self).session_info()
-        config = self.env['ir.config_parameter'].sudo()
-        result.update({
-            "leaflet.tile_url": config.get_param('leaflet.tile_url', default=''),
-            "leaflet.copyright": config.get_param('leaflet.copyright', default=''),
-        })
+        config = self.env["ir.config_parameter"].sudo()
+        result.update(
+            {
+                "leaflet.tile_url": config.get_param("leaflet.tile_url", default=""),
+                "leaflet.copyright": config.get_param("leaflet.copyright", default=""),
+            }
+        )
         return result
--- web_view_leaflet_map/static/src/js/view/map/map_renderer.js
+++ web_view_leaflet_map/static/src/js/view/map/map_renderer.js
@@ -1,19 +1,19 @@
-odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
-    'use strict';
+odoo.define("web_view_leaflet_map.MapRenderer", function (require) {
+    "use strict";
 
-    var AbstractRenderer = require('web.AbstractRenderer');
-    var session = require('web.session');
-    var field_utils = require('web.field_utils');
+    var AbstractRenderer = require("web.AbstractRenderer");
+    var session = require("web.session");
+    var field_utils = require("web.field_utils");
 
     var MapRenderer = AbstractRenderer.extend({
-        tagName: 'div',
-        className: 'o_leaflet_main_container',
+        tagName: "div",
+        className: "o_leaflet_main_container",
 
         init: function (parent, state, params) {
             this._super.apply(this, arguments);
 
-            this.leaflet_tile_url = session['leaflet.tile_url'];
-            this.leaflet_copyright = session['leaflet.copyright'];
+            this.leaflet_tile_url = session["leaflet.tile_url"];
+            this.leaflet_copyright = session["leaflet.copyright"];
             this.default_zoom = params.arch.attrs.default_zoom || 7;
             this.max_zoom = params.arch.attrs.max_zoom || 19;
             this.zoom_snap = params.arch.attrs.zoom_snap | 1;
@@ -31,11 +31,13 @@ odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
         start: function () {
             var self = this;
             var self_super = this._super;
-            return this._initDefaultPosition().then(function () {
-                self._initMap();
-            }).then(function () {
-                return self_super.apply(self, arguments);
-            })
+            return this._initDefaultPosition()
+                .then(function () {
+                    self._initMap();
+                })
+                .then(function () {
+                    return self_super.apply(self, arguments);
+                });
         },
 
         _render: function () {
@@ -57,55 +59,70 @@ odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
             // the map. See.
             // https://github.com/Leaflet/Leaflet/issues/3002#issuecomment-93836022
             return this._super.apply(this, arguments).then(function () {
-                window.setTimeout(function(){
-                      self.leaflet_map.invalidateSize();
-                },1);
-            })
+                window.setTimeout(function () {
+                    self.leaflet_map.invalidateSize();
+                }, 1);
+            });
         },
 
-        _renderRecord: function(record) {
+        _renderRecord: function (record) {
             var self = this;
-            var latlng = L.latLng(record.data[this.field_latitude], record.data[this.field_longitude]);
+            var latlng = L.latLng(
+                record.data[this.field_latitude],
+                record.data[this.field_longitude]
+            );
             // Display only records that have a valid position
             if (latlng.lat != 0 && latlng.lng != 0) {
-                // create marker
+                // Create marker
                 var markerOptions = this._prepareMarkerOptions(record);
-                var marker = L.marker(latlng, markerOptions).addTo(this.leaflet_layer_group);
+                var marker = L.marker(latlng, markerOptions).addTo(
+                    this.leaflet_layer_group
+                );
 
                 // Create Popup and attach an event onclick
                 var popup = L.popup().setContent(this._preparePopUpData(record));
 
                 marker.bindPopup(popup).on("popupopen", () => {
-                    $(".o_map_selector").parent().parent().click({model_name: record.model, res_id: record.data["id"], current_object: self}, self._onClickLeafletPopup);
+                    $(".o_map_selector")
+                        .parent()
+                        .parent()
+                        .click(
+                            {
+                                model_name: record.model,
+                                res_id: record.data.id,
+                                current_object: self,
+                            },
+                            self._onClickLeafletPopup
+                        );
                 });
-
             }
         },
 
         _onClickLeafletPopup: function (ev) {
             ev.preventDefault();
-            ev.data.current_object.trigger_up('switch_view', {
-                view_type: 'form',
+            ev.data.current_object.trigger_up("switch_view", {
+                view_type: "form",
                 res_id: ev.data.res_id,
                 model: ev.data.model_name,
             });
         },
 
-        _prepareMarkerIcon: function(record) {
+        _prepareMarkerIcon: function (record) {
             var myIcon = L.icon({
-                iconUrl: session.url('/web/image', {
+                iconUrl: session.url("/web/image", {
                     model: record.model,
                     id: JSON.stringify(record.data.id),
                     field: this.field_marker_icon_image,
-                    // unique forces a reload of the image when the record has been updated
-                   unique: field_utils.format.datetime(record.data.__last_update).replace(/[^0-9]/g, ''),
+                    // Unique forces a reload of the image when the record has been updated
+                    unique: field_utils.format
+                        .datetime(record.data.__last_update)
+                        .replace(/[^0-9]/g, ""),
                 }),
                 className: "leaflet_marker_icon",
                 iconSize: [this.marker_icon_size_x, this.marker_icon_size_y],
                 popupAnchor: [this.marker_popup_anchor_x, this.marker_popup_anchor_y],
             });
             return myIcon;
-
         },
 
         _prepareMarkerOptions: function (record) {
@@ -114,7 +131,7 @@ odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
                 title: record.data[this.field_title],
                 alt: record.data[this.field_title],
                 riseOnHover: true,
-            }
+            };
             if (icon) {
                 result.icon = icon;
             }
@@ -123,10 +140,15 @@ odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
 
         _preparePopUpData: function (record) {
             return (
-                "<div class='o_map_selector' res_id='" + record.data["id"] + "'>"
-                + "<b>" + record.data[this.field_title] + "</b><br/>"
-                + " - " + record.data[this.field_address]
-                + "</div>"
+                "<div class='o_map_selector' res_id='" +
+                record.data.id +
+                "'>" +
+                "<b>" +
+                record.data[this.field_title] +
+                "</b><br/>" +
+                " - " +
+                record.data[this.field_address] +
+                "</div>"
             );
         },
 
@@ -134,11 +156,11 @@ odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
             var self = this;
             return this._rpc({
                 model: "res.users",
-                method: 'get_default_leaflet_position',
+                method: "get_default_leaflet_position",
                 args: [this.state.model],
             }).then(function (result) {
                 self.default_lat_lng = L.latLng(result.lat, result.lng);
-            })
+            });
         },
 
         _initMap: function () {
@@ -153,9 +175,7 @@ odoo.define('web_view_leaflet_map.MapRenderer', function (require) {
             }).addTo(this.leaflet_map);
             this.$el.append($mainDiv);
         },
-
     });
 
     return MapRenderer;
-
 });
