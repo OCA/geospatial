1a4ab4860df8e40591f747ae90f6a7cd68929c7a
yibudak <yibudak@gmail.com>
2023-11-30 14:11:57 +0300

    [16.0][MIG] web_view_leaflet_map

---
+++ web_view_leaflet_map/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Leaflet Map View (OpenStreetMap)",
+    "summary": "Integrate leaflet.js librairy with odoo"
+    " and add new 'leaflet_map' view, to display markers.",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "author": "GRAP, Yiğit Budak, Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain", "yibudak"],
+    "website": "https://github.com/OCA/geospatial",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        "base_geolocalize",
+    ],
+    "data": [
+        "data/ir_config_parameter.xml",
+        # "views/templates.xml",
+    ],
+    "demo": [
+        "demo/ir_config_parameter.xml",
+    ],
+    "installable": True,
+    "uninstall_hook": "uninstall_hook",
+    "assets": {
+        "web.assets_backend": [
+            "web_view_leaflet_map/static/src/css/web_view_leaflet_map.css",
+            "web_view_leaflet_map/static/lib/leaflet/leaflet.css",
+            "web_view_leaflet_map/static/lib/leaflet/leaflet.js",
+            "web_view_leaflet_map/static/src/js/view/map/map_renderer.js",
+            "web_view_leaflet_map/static/src/js/view/map/map_view.js",
+            "web_view_leaflet_map/static/src/js/view/view_registry.js",
+        ],
+    },
+}
---
+++ web_view_leaflet_map/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Sylvain LE GAL (https://www.twitter.com/legalsylvain)
+* Yiğit Budak (https://github.com/yibudak)
