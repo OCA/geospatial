ffc58731326ba62a51e845c2fae91c2977a84c90
Hadrien Huvelle <hhu@baboum.be>
2024-02-06 07:35:15 +0100

    Init website_geoengine_store_locator  module

---
+++ website_geoengine_store_locator/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2011-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Geospatial Website store locator",
+    "version": "16.0.1.0.0",
+    "description": "Adds a website widget to display a map with the store locations.",
+    "category": "GeoBI",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/geospatial",
+    "depends": ["base_geoengine", "website", "partner_store"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
