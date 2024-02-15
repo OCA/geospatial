25684d0e6bbaa2bd191d365ec54c7c682ddcefda
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-02-15 08:02:58 +0100

    Some more

--- website_geoengine_store_locator/static/src/js/snippets/s_openstreetmap/search.js
+++ website_geoengine_store_locator/static/src/js/snippets/s_openstreetmap/search.js
@@ -2,24 +2,28 @@
 
     /**
      * Create a standard symbol for a POI
-     * @param {number} radius the radius of the top circle
      * @param {number} height the height of the circle center
+     * @param {number} radius the radius of the top circle
      * @param {string} fillColor the fill color of the symbol
      * @param {number} strokeWidth the stroke width of the symbol
      * @param {string} strokeColor the stroke color of the symbol
      * @param {number} centerRadius the radius of the center point
-     * @param {string} centerColor the fill color of the center point
+     * @param {string} centerFillColor the fill color of the center point
+     * @param {number} centerStrokeWidth the stroke width of the center point
+     * @param {string} centerStrokeColor the stroke color of the center point
      * @returns
      */
-    function buildIcon(
-        radius,
+      function buildIcon(
         height,
+        radius,
         fillColor,
         strokeWidth,
         strokeColor,
         centerRadius,
-        centerColor
-    ) {
+        centerFillColor,
+        centerStrokeWidth,
+        centerStrokeColor
+      ) {
         const canvas = document.createElement("canvas");
         canvas.width = radius * 2 + strokeWidth;
         canvas.height = radius + height + strokeWidth;
@@ -42,29 +46,28 @@
         context.moveTo(line2End[0], line2End[1]);
         context.lineTo(linesStart[0], linesStart[1]);
         context.lineTo(line1End[0], line1End[1]);
-        context.arc(
-            circleCenter[0],
-            circleCenter[1],
-            radius,
-            Math.PI / 2 + alpha,
-            Math.PI / 2 - alpha
-        );
+        context.arc(circleCenter[0], circleCenter[1], radius, Math.PI / 2 + alpha, Math.PI / 2 - alpha);
         context.lineTo(line2End[0], line2End[1]);
         context.stroke();
-        context.fill();
+        context.moveTo(circleCenter[0] + centerRadius, circleCenter[1]);
+        context.arc(circleCenter[0], circleCenter[1], centerRadius, 0, Math.PI * 2, true);
         context.closePath();
+        context.fill();
 
-        context.fillStyle = centerColor;
+        context.fillStyle = centerFillColor;
+        context.strokeStyle = centerStrokeColor;
+        context.lineWidth = centerStrokeWidth;
         context.beginPath();
         context.arc(circleCenter[0], circleCenter[1], centerRadius, 0, Math.PI * 2);
-        context.fill();
         context.closePath();
+        context.stroke();
+        context.fill("evenodd");
 
         return new ol.style.Icon({
-            img: canvas,
-            anchor: [0.5, 1],
+          img: canvas,
+          anchor: [0.5, 1],
         });
-    }
+      }
 
     /**
      * Normalize a string by removing accents and lowercasing it for the search.
@@ -137,13 +140,15 @@
         if (!foundStyle) {
             foundStyle = new ol.style.Style({
                 image: buildIcon(
-                    10,
                     25,
+                    10,
                     "rgba(0, 0, 255, 0.2)",
                     1.5,
                     "rgb(0, 0, 255)",
-                    2.5,
-                    "rgb(50, 50, 255)"
+                    5,
+                    "rgba(0, 0, 0, 0)"
+                    1.5,
+                    "rgb(0, 0, 255)",
                 ),
             });
         }
