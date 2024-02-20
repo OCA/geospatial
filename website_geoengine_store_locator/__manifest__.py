5f48e76390f2925d32780edd0c04b26aa566ea65
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-02-20 17:46:24 +0100

    Use odoo style

--- website_geoengine_store_locator/__manifest__.py
+++ website_geoengine_store_locator/__manifest__.py
@@ -15,7 +15,8 @@
     ],
     "assets": {
         "web.assets_frontend": [
-            "/website_geoengine_store_locator/static/lib/node_modules/ol/dist/ol.js",
+            "website_geoengine_store_locator/static/lib/node_modules/ol/dist/ol.js",
+            "website_geoengine_store_locator/static/lib/node_modules/jquery-flexdatalist/jquery.flexdatalist.js",
             "website_geoengine_store_locator/static/src/scss/snippets/s_openlayer_store_locator/frontend.scss",
             "website_geoengine_store_locator/static/src/js/snippets/s_openlayer_store_locator/frontend.js",
             "website_geoengine_store_locator/static/src/js/snippets/s_openlayer_store_locator/popover.js",
