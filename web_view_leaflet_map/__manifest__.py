e543551012f6136f36525bbe78bbc186a37115a0
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-10-12 22:15:00 +0200

    [REF] split web_view_leaflet_map into web_view_leaflet_map and web_leaflet_lib, to be used by other modules, like 'web_widget_map'. (see : https://github.com/OCA/web/pull/2953/)

--- web_view_leaflet_map/__manifest__.py
+++ web_view_leaflet_map/__manifest__.py
@@ -4,9 +4,8 @@
 
 {
     "name": "Leaflet Map View (OpenStreetMap)",
-    "summary": "Integrate leaflet.js librairy with odoo"
-    " and add new 'leaflet_map' view, to display markers.",
-    "version": "16.0.1.0.0",
+    "summary": "Add new 'leaflet_map' view, to display markers.",
+    "version": "16.0.2.0.0",
     "development_status": "Alpha",
     "author": "GRAP, Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
@@ -15,12 +14,7 @@
     "category": "Extra Tools",
     "depends": [
         "base_geolocalize",
-    ],
-    "data": [
-        "data/ir_config_parameter.xml",
-    ],
-    "demo": [
-        "demo/ir_config_parameter.xml",
+        "web_leaflet_lib",
     ],
     "assets": {
         "web.assets_backend": [
@@ -28,8 +22,6 @@
             "web_view_leaflet_map/static/src/js/view/map/map_renderer.js",
             "web_view_leaflet_map/static/src/js/view/map/map_view.js",
             "web_view_leaflet_map/static/src/css/web_view_leaflet_map.css",
-            "/web_view_leaflet_map/static/lib/leaflet/leaflet.css",
-            "/web_view_leaflet_map/static/lib/leaflet/leaflet.js",
         ],
     },
     "installable": True,
