51269fc399a8642fc59b5050cf74b1fc5e006132
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2024-10-16 04:27:55 +0000

    base_geoengine dropped, big mistake.

--- base_geoengine/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import models
-from . import expressions
-from . import fields
-from . import geo_convertion_helper
-from . import geo_operators
-from .geo_db import init_postgis
--- base_geoengine/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright 2023 ACSONE SA/NV
-from . import test_model
