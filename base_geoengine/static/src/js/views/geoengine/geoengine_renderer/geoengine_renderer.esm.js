db8acb3b528d17983db808ffe5c0b2e31102574d
anusrinps96 <aprakhasam@nps100.com>
2025-08-07 08:30:47 +0200

    [IMP] base_geoengine: Include edit button in Popup
       Added edit button in popup of record shown in geoengine
    view which allows user to edit values of record from
    geoengine view.

--- base_geoengine/static/src/js/views/geoengine/geoengine_renderer/geoengine_renderer.esm.js
+++ base_geoengine/static/src/js/views/geoengine/geoengine_renderer/geoengine_renderer.esm.js
@@ -649,6 +649,14 @@ export class GeoengineRenderer extends Component {
     }
 
     /**
+     * When you click on the edit button, it calls the controller's
+     * editRecord method.
+     */
+    onEditButtonClicked() {
+        this.props.editRecord(this.record.resModel, this.record.resId);
+    }
+
+    /**
      * Allows you to change the visibility of layers. This method is called
      * when the user changes raster layers.
      */
@@ -1309,6 +1317,7 @@ GeoengineRenderer.props = {
     archInfo: {type: Object},
     data: {type: Object},
     openRecord: {type: Function},
+    editRecord: {type: Function},
     editable: {type: Boolean, optional: true},
     updateRecord: {type: Function},
     onClickDiscard: {type: Function},
