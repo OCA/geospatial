9d1e519b4ba99b670e489c86f8352472d1f98053
Mourad <mourad.elhadj.mimoune@akretion.com>
2025-10-21 12:24:17 +0200

    [MIG] web_view_leaflet_map_partner: Migration to 18.0

--- web_view_leaflet_map_partner/__manifest__.py
+++ web_view_leaflet_map_partner/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Leaflet Map View for Partners (OpenStreetMap)",
     "summary": "TODO",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "author": "GRAP, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/geospatial",
