f3ce3d92c53638a3417dd4612d24b072986f602d
Mourad <mourad.elhadj.mimoune@akretion.com>
2025-10-21 12:40:44 +0200

    [MIG] web_leaflet_lib: Migration to 18.0

--- web_leaflet_lib/__manifest__.py
+++ web_leaflet_lib/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Leaflet Javascript Library",
     "summary": "Bring leaflet.js librairy in odoo.",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "GRAP, Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
     "website": "https://github.com/OCA/geospatial",
