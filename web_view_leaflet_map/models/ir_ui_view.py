386eed2c73d9dd4c3477b77312a834b699caf255
Thibault Rey <thibault.rey@akretion.com>
2025-11-05 12:02:15 +0100

    [MIG] web_view_leaflet_map: fix view mode declaration for 18.0 migration

--- web_view_leaflet_map/models/ir_ui_view.py
+++ web_view_leaflet_map/models/ir_ui_view.py
@@ -10,3 +10,6 @@ class IrUiView(models.Model):
     _inherit = "ir.ui.view"
 
     type = fields.Selection(selection_add=[("leaflet_map", "Leaflet Map")])
+
+    def _get_view_info(self):
+        return {"leaflet_map": {"icon": "fa fa-map-o"}} | super()._get_view_info()
