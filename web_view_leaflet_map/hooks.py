730a18524d47a06ec38807e1bee5eee6698ceac5
Eng. Mohammad Abbadi <mohammad.abbadi@outlook.com>
2025-12-30 14:01:01 +0300

    Update hooks.py

--- web_view_leaflet_map/hooks.py
+++ web_view_leaflet_map/hooks.py
@@ -15,4 +15,4 @@ def uninstall_hook(env):
         "SET view_mode=replace(view_mode, 'leaflet_map,', '')"
         "WHERE view_mode LIKE '%leaflet_map,%';"
     )
-    env.cr.execute("DELETE FROM ir_act_window " "WHERE view_mode = 'leaflet_map';")
+    env.cr.execute("DELETE FROM ir_act_window WHERE view_mode = 'leaflet_map';")
