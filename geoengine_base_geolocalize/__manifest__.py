685caad4b17fec02f46910bc824cac2dcb37b0bc
Cédric Paradis <cparadis@impressfoods.com>
2026-04-10 15:15:44 +0000

    [MIG] geoengine_base_geolocalize: Migration to 19.0

--- geoengine_base_geolocalize/__manifest__.py
+++ geoengine_base_geolocalize/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Geospatial support for base_geolocalize",
-    "version": "17.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "GeoBI",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
