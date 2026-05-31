5080da5b87d9794c5f5a500bba0ab452d76505c3
Jonasaaaa <jonas.eykens@isoproc.be>
2026-05-31 10:26:53 +0200

    [FIX] web_view_leaflet_map: show the search bar on the map view
    The map controller embedded Layout but never placed a SearchBar, so the
    control panel had no search/filter bar (filters could only be set from another
    view). Render SearchBar in the layout-actions slot and wire the responsive
    search-bar toggler, mirroring the standard map view.

--- web_view_leaflet_map/static/src/views/leaflet_map/leaflet_map_controller.esm.js
+++ web_view_leaflet_map/static/src/views/leaflet_map/leaflet_map_controller.esm.js
@@ -1,6 +1,8 @@
 import {Component, useRef} from "@odoo/owl";
 import {Layout} from "@web/search/layout";
 import {MapRenderer} from "./leaflet_map_renderer.esm";
+import {SearchBar} from "@web/search/search_bar/search_bar";
+import {useSearchBarToggler} from "@web/search/search_bar/search_bar_toggler";
 import {executeButtonCallback} from "@web/views/view_button/view_button_hook";
 
 /**
@@ -8,10 +10,11 @@ import {executeButtonCallback} from "@web/views/view_button/view_button_hook";
  */
 export class MapController extends Component {
     static template = "web_view_leaflet_map.MapView";
-    static components = {Layout, MapRenderer};
+    static components = {Layout, MapRenderer, SearchBar};
 
     setup() {
         this.rootRef = useRef("root");
+        this.searchBarToggler = useSearchBarToggler();
     }
 
     async onClickCreate() {
